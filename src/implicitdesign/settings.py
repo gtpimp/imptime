b00346581960a83bc36ebc51dd81b4ebd90de764
gtp <garethpriede@gmail.com>
2012-08-14 22:08:10 +0200

    added 404 and 500 files

--- src/implicitdesign/settings.py
+++ src/implicitdesign/settings.py
@@ -194,6 +194,10 @@ EMACSIMPORTER_RATES = { "test":{"id-test.org":50,
                                 "id-koen.org":100,
                                 "id-fonk.org":200} }
 
+# # These urls may need to be specified on a production server when the site it not hosted at the root domain.
+#LOGIN_URL=
+#LOGOUT_URL=
+
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
 # the site admins on every HTTP 500 error when DEBUG=False.
