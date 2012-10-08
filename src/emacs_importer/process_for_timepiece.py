e3a09f6c18c0e0ecc4a8d0bf1dc1ce2a3b526a7e
gtp <garethpriede@gmail.com>
2012-10-08 23:49:50 +0200

    fix for issue regex

--- src/emacs_importer/process_for_timepiece.py
+++ src/emacs_importer/process_for_timepiece.py
@@ -310,7 +310,7 @@ class Processor(object):
     def get_issue_id(self, clocktable_entry):
         raw_issue = clocktable_entry['issue']
         issue_id = None
-        for regex in [ "issue([^ ])+", "issue *#([^ ]+)" ]:
+        for regex in [ "issue([^ :]+)", "issue *#([^ ]+)" ]:
             match_object = re.compile(regex).search(raw_issue)
             if match_object and match_object.groups() != 0:
                 try:
