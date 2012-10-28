1d4dc7d4263936563ec1506d4954e72039ba77f4
gtp <garethpriede@gmail.com>
2012-10-28 20:01:48 +0200

    fixed bug in the output of the import

--- src/emacs_importer/views.py
+++ src/emacs_importer/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
 import os
+import pprint
 from implicitdesign import settings
 from django.contrib.auth.decorators import login_required, permission_required
 from tasks import import_timesheets_from_emacs_task, import_timesheets_from_emacs
@@ -12,7 +13,7 @@ def do_import(request, template="import.html", context=None):
 
     if 'immediate' in request.GET:
         status = import_timesheets_from_emacs()
-        context['status_msg'] = simplejson.dumps(status, sort_keys=True, indent=2)
+        context['status_msg'] = pprint.pformat(status, indent=2).replace(" ","&nbsp;").replace("\n", "<br/>")
         context['msg'] = 'import task run'
     else:
         task = import_timesheets_from_emacs_task.delay()
