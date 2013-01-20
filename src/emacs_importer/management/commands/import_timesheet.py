93598d35429ce2cd50310f790d6af50a566c431a
gtp <garethpriede@gmail.com>
2013-01-21 01:20:56 +0200

    blah

--- src/emacs_importer/management/commands/import_timesheet.py
+++ src/emacs_importer/management/commands/import_timesheet.py
@@ -5,17 +5,10 @@ import os
 from implicitdesign import settings
 
 class Command(BaseCommand):
-    args = 'users'
 
     help = '''Imports the timesheets from emacs into the timepiece
-    website. Users is a comma separated list of usernames to process
-    (or empty for all)'''
+    website. '''
 
-    def handle(self, users=None, *args, **options):
-        if users is not None:
-            users = users.split(",")
-        status = import_timesheets_from_emacs(users=users)
-
+    def handle(self, *args, **options):
+        import_timesheets_from_emacs()
         print("Import complete")
-        print("Unknown entries saved to : %s" % os.path.join(settings.MEDIA_ROOT,"unknown_redmine_entries"))
-
