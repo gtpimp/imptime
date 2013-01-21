ce40da1bad816e3dfa28034da1d6e1597edce9c3
gtp <garethpriede@gmail.com>
2013-01-21 21:48:13 +0200

    blah

--- scripts/import_timesheets.sh
+++ scripts/import_timesheets.sh
@@ -1,6 +1,38 @@
 #!/bin/bash
 
-cd ~/impwebsite
+function update_git {
+  USER=$1
+  cd /home/timesheets/input/$USER
+  git reset --hard HEAD
+  git pull origin master
+}
+
+function graph {
+  USER=$1
+  python manage.py email_graphs --username $USER --test 1
+}
+
+update_git alec
+update_git david
+update_git gtp
+update_git stephan
+
+cd /home/timesheets/input/alec
+git reset --hard HEAD
+
+cd /home/website/impwebsite
 . ./venv/bin/activate
 cd src
 python manage.py import_timesheet
+
+graph alec
+graph gtp
+graph david
+graph stephan
+graph mitzie
+graph ben
+
+
+
+
+
