3e2f87d012721e1649925260b223c49950111049
gtp <garethpriede@gmail.com>
2013-03-01 11:33:08 +0200

    blah

--- scripts/import_timesheets.sh
+++ scripts/import_timesheets.sh
@@ -12,7 +12,7 @@ function graph {
   USER=$1
   echo "graphing $USER..."
   #python manage.py email_graphs --username $USER --test 1
-  python manage.py email_graphs --username $USER
+  python manage.py email_graphs --username $USER --settings "management_settings"
 }
 
 update_git alec
@@ -25,7 +25,7 @@ cd /home/timesheet
 cd src
 
 echo "importing timesheet..."
-python manage.py import_timesheet
+python manage.py --settings "management_settings" import_timesheet 
 
 graph alec
 graph gtp
---
+++ src/implicitdesign/management_settings.py
@@ -0,0 +1,3 @@
+from settings import *
+LOGGING_CONFIG = None
+
