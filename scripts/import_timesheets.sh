486163278f360638594aec89efeffc8c36e763ca
gtp <garethpriede@gmail.com>
2013-03-12 19:20:06 +0200

    added keith

--- scripts/import_timesheets.sh
+++ scripts/import_timesheets.sh
@@ -19,6 +19,7 @@ update_git alec
 update_git david
 update_git gtp
 update_git stephan
+update_git keith
 
 cd /home/timesheet
 . ./venv/bin/activate
@@ -34,4 +35,4 @@ graph stephan
 graph mitzie
 graph ben
 graph etienne
-
+graph keith
