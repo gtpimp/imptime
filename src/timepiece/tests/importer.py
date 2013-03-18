48ee01196544010f46e68e570228e2a5a8adec91
Keith <keith@implicitdesign.co.za>
2013-03-18 13:41:49 +0200

    Issue682: Updated import entries to better validate sprint names.
    src/timepiece/forms.py b/src/timepiece/forms.py
    When import is done, all non-alphanums are removed and checked
    against sprint names in db. Same is done to names in db.
    If duplicates names are found, the user is alerted.
    
    Added tests to src/timepiece/tests/importer.py and disabled all
    other tests.

---
+++ src/timepiece/tests/importer.py
@@ -0,0 +1,28 @@
+from timepiece.tests.base import TimepieceDataTestCase
+from timepiece import models as timepiece
+from timepiece import utils
+from django.contrib.auth.models import User, Permission
+from django.core.urlresolvers import reverse
+
+class ImportProjectTestCase(TimepieceDataTestCase):
+    def test_import_invalid(self):
+        self.client.login(username=self.user.username, password='abc')
+        response = self.client.post(reverse('timepiece.views.import_entries'),
+                                    {'raw_entries': "business|project"},)
+        self.client.logout()
+        self.assertTrue(response.context['errors'])
+        self.assertEqual(response.context['errors'][0]['msg'],
+                         'need more than 2 values to unpack')
+
+    def test_import_project_validation_modify_name(self):
+        self.client.login(username=self.superuser.username, password='abc')
+        response = self.client.post(reverse('timepiece.views.import_entries'),
+                                    {'raw_entries': "13-Jan-13|Example Business|ExampleProject1|Description|8.5"},)
+        self.client.logout()
+        self.assertTrue(response.context['num_entries_created'])
+
+    def test_import_project_does_not_exist(self):
+        self.client.login(username=self.superuser.username, password='abc')
+        response = self.client.post(reverse('timepiece.views.import_entries'),
+                                    {'raw_entries': "13-Jan-13|Example Business|EaeouoaeuxampleProject1|Description|8.5"},)
+        self.assertTrue(response.context['errors'])
