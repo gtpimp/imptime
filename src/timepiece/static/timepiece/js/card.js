fd527c3217cc584680b03e3c78168c26c91cdcbf
gtp <garethpriede@gmail.com>
2013-06-15 22:07:20 +0200

    fixed editing rates on project card page

--- src/timepiece/static/timepiece/js/card.js
+++ src/timepiece/static/timepiece/js/card.js
@@ -7,13 +7,14 @@ imp.project_card_thinking = function(el) {
 
 imp.popup_page = function(url) {
 
-  $(".project_card_dialog_container").dialog( { open: function(event, ui) {
+    if (imp.popup_dialog) {
+	$(".project_card_dialog_container").find(".dialog_content").load(url);
+    } else {
+	$(".project_card_dialog_container").dialog( { open: function(event, ui) {
 						    $(".project_card_dialog_container").find(".dialog_content").load(url);
-						},
-						close: function(ev, ui) {
-						    $(this).remove();
 						}
 					      });
+    }
 						  
 };
 
