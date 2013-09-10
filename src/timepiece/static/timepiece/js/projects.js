d1f7046c5a18f9414bd1b7736f3cc1b32b319d6d
Stephan van Ellewee <stephan@implicitdesign.co.za>
2013-09-10 11:40:36 +0200

    toggle

--- src/timepiece/static/timepiece/js/projects.js
+++ src/timepiece/static/timepiece/js/projects.js
@@ -7,11 +7,15 @@ imp.projects.expand_issues = function(element, expand_url) {
     var project_contents = parent_table.find(".project_contents");
     var area_to_insert = project_contents.find(".information");
 
+    if (area_to_insert.find(".project_detail").length == 0) {
     $.ajax({type:"GET",
             url: expand_url,
-            success: function(data) {
+            success: function(data) {                                
                 area_to_insert.append($(data))
             }
            });
+    } else {
+        area_to_insert.find(".project_detail").remove();
+    }
 
 };
