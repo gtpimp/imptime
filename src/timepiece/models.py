347c9df73f217372fdeb4a80e014d0229b3ec6f1
Stephan van Ellewee <stephan@implicitdesign.co.za>
2013-09-11 16:51:40 +0200

    added some migrations

--- src/timepiece/models.py
+++ src/timepiece/models.py
@@ -176,6 +176,10 @@ class Business(models.Model):
         else:
             return None
 
+
+class Feature(models.Model):
+    business = models.ForeignKey(Business)
+
 class ProjectQuerySet(QuerySet):
     def filter_by_logged_in_user(self, user):
         """ restricts entries to those belonging to projects the given
@@ -1833,6 +1837,7 @@ class Issue(models.Model):
     description = models.TextField(blank=True)
     story_points = models.FloatField(null=True,blank=True)    
     order = models.IntegerField(null=True,blank=True)
+    feature = models.OneToOneField("Issue",blank=True,null=True,related_name='issue')
 
     @classmethod
     def get_last_issue_number(self):
