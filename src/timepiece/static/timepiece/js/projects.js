48b74493ce9a372870eca65b637f79fd4baaa0cc
Stephan van Ellewee <stephan@implicitdesign.co.za>
2013-09-19 12:45:09 +0200

    removed unused code

--- src/timepiece/static/timepiece/js/projects.js
+++ src/timepiece/static/timepiece/js/projects.js
@@ -63,7 +63,7 @@ imp.projects.show_project_card_as_popup = function (event) {
                 var current_project_id = project_card_top_level.attr('project_id');
                 var current_business_id = project_card_top_level.attr('business_id');
                 project_card_top_level.parent().remove();                
-                if ((project_id ===current_project_id) && (business_id === current_business_id)){
+                if ((project_id === current_project_id) && (business_id === current_business_id)){
                     return false;
                 }
             }
@@ -80,43 +80,6 @@ imp.projects.show_project_card_as_popup = function (event) {
     event.stopPropagation();
     return false;
 };
-
-// imp.projects.show_project_card_as_popup = function(current_element, destination_dom_tag, project_card_url, business_id, project_id) {
-//     var destination = $(destination_dom_tag);
-//     var response = $.ajax({ type:"GET",
-//                             url: project_card_url,                            
-//                           });
-//     var for_data_of_response = function (destination_tag) {
-
-//        var _bus_id = business_id;
-//         var _proj_id = project_id;
-//         var _dest_tag = destination_tag;
-
-//         return function(data) { 
-//             var project_id = _proj_id;
-//             var business_id = _bus_id;
-//             var destination = $(_dest_tag);
-//             var project_card_top_level = $(document).find(".project_card_top_level");
-//             if (project_card_top_level.length != 0) {
-//                 var current_project_id = project_card_top_level.attr('project_id');
-//                 var current_business_id = project_card_top_level.attr('business_id');
-//                 project_card_top_level.parent().remove();                
-//                 if ((project_id ===current_project_id) && (business_id === current_business_id)){
-//                     return false;
-//                 }
-//             }
-
-//             destination.append($(data)); 
-//             destination.css("background-color","whitesmoke");
-//             destination.css("position","fixed");
-//             destination.css("z-index","300");
-//             destination.draggable();
-//         }
-//     };
-
-//     response.done(  for_data_of_response(destination_dom_tag)  );
-//     return false;
-// };
 
 imp.projects._make_load_for_data = function ( done_data_function_handler ) {
     var _done_data_function_handler = done_data_function_handler;
