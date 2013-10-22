10b1da6190d222ccc85c84d4663d963ce483e371
gtp <garethpriede@gmail.com>
2013-10-22 20:05:55 +0200

    blah2

--- src/emacs_importer/extract_for_timepiece.py
+++ src/emacs_importer/extract_for_timepiece.py
@@ -62,8 +62,7 @@ class Extractor(object):
             if hours_before != hours_after:
                 project = Project.objects.get(pk=p_id)
                 if not project.is_open:
-                    pass
-                    #raise Exception("Trying to add entries to a closed project [%s - %s]" % (project.business.name, project))
+                    raise Exception("Trying to add entries to a closed project [%s - %s]" % (project.business.name, project))
 
     def _handle_file(self, dirname, fname):
         is_valid_timesheet_file = fname[-4:] == ".org" and fname[0] != "." and fname[0] != "#"
