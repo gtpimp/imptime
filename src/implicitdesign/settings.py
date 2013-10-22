6666004a96d08255cbd91863e6953ad103ee76b0
gtp <garethpriede@gmail.com>
2013-10-22 14:17:55 +0200

    added missing admin styyles

--- src/implicitdesign/settings.py
+++ src/implicitdesign/settings.py
@@ -218,7 +218,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.humanize',
-    #'grappelli',
+    'grappelli',
     'filebrowser',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
