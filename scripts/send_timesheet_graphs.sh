e39eac54e347ce0ce63f1c626d92463ee0788422
gtp <garethpriede@gmail.com>
2014-02-03 11:19:04 +0200

    split out graphing script

---
+++ scripts/send_timesheet_graphs.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+function graph {
+  USER=$1
+  echo "graphing $USER..."
+  #python manage.py email_graphs --username $USER --test 1
+  python manage.py email_graphs --username $USER --settings "implicitdesign.management_settings"
+}
+
+cd /home/timesheet
+. ./venv/bin/activate
+cd src
+
+graph alec
+graph gtp
+graph david
+graph mitzie
+graph ben
+graph etienne
+graph keith
