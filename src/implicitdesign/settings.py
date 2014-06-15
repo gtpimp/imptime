011b2bfb55ed8ef00541b6df56942880c04de8db
gtp <garethpriede@gmail.com>
2014-06-15 23:10:16 +0200

    minor tweaks

--- src/implicitdesign/settings.py
+++ src/implicitdesign/settings.py
@@ -232,6 +232,7 @@ INSTALLED_APPS = (
     'dateutil',
     'djcelery',
     'colorful',
+    'endless_pagination',
 
     'timepiece',
     'south',
@@ -243,6 +244,8 @@ INSTALLED_APPS = (
 )
 
 PAGINATION_DEFAULT_PAGINATION=50
+ENDLESS_PAGINATION_PER_PAGE=PAGINATION_DEFAULT_PAGINATION
+ENDLESS_PAGINATION_ADD_NOFOLLOW=True #from endless docs: Set to True if your SEO alchemist wants search engines not to follow pagination links.
 
 INVOICE_PAYMENT_DAYS=30
 
