0bf9c090f289c806afaaf442dc20a7d8f309de2b
gtp <garethpriede@gmail.com>
2014-06-16 18:59:56 +0200

    fix for vat column in invoices

--- src/invoicing/models.py
+++ src/invoicing/models.py
@@ -32,7 +32,7 @@ class InvoiceQuerySet(QuerySet):
                (self.filter(client__taxable=False).aggregate(Sum('items__total_cost'))['items__total_cost__sum'] or 0)
 
     def vat(self):
-        return self.cost_with_vat - self.cost
+        return self.cost_with_vat() - self.cost()
 
     def cost(self):
         return self.aggregate(Sum('items__total_cost'))['items__total_cost__sum']
