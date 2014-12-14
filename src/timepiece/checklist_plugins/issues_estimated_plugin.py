f54d41fa5122dac0fbff97470b9eb62e311352e7
Gareth Priede <gtp@implicitdesign.co.za>
2014-12-14 21:30:12 +0200

    added estimates points checklist plugin

---
+++ src/timepiece/checklist_plugins/issues_estimated_plugin.py
@@ -0,0 +1,17 @@
+from base_plugin import BasePlugin
+from django.db.models import Q, Avg, Sum, Max, Min
+
+class IssuesEstimatesPlugin(BasePlugin):
+
+    def __init__(self, business, name="estimates", *args, **kwargs):
+        super(IssuesEstimatesPlugin, self).__init__(business=business, name=name, *args, **kwargs)
+
+    def check_for_problems(self):
+        from timepiece import models as timepiece
+        problems = []
+        projects = timepiece.Project.objects.all().filter(business=self.business).filter_open().filter_in_dev_or_pending()
+        unestimated_issues = timepiece.Issue.objects.all().filter(project__in=projects).annotate(estimate=Sum('issue_points__points')).filter(estimate=0)
+        for issue in unestimated_issues:
+            problems.append(self._create_problem_item(msg="Issue %s in sprint %s is not assigned"%(issue.number, issue.project), issue=issue))
+        return problems
+    
--- src/timepiece/checklist_plugins/registry.py
+++ src/timepiece/checklist_plugins/registry.py
@@ -3,6 +3,7 @@ from rates_plugin import RatesPlugin
 from budget_present_plugin import BudgetPresetPlugin
 from over_budget_plugin import OverBudgetPlugin
 from missing_timesheet_entries_plugin import MissingTimesheetEntriesPlugin
+from issues_estimated_plugin import IssuesEstimatesPlugin
 
 finance_plugin_classes = [ RatesPlugin, BudgetPresetPlugin, OverBudgetPlugin ]
 def get_finance_plugins(business):
@@ -12,6 +13,6 @@ dev_plugin_classes = [  ]
 def get_dev_plugins(business):
     return [ x(business) for x in dev_plugin_classes ]
 
-traffic_plugin_classes = [ MissingTimesheetEntriesPlugin, ]
+traffic_plugin_classes = [ MissingTimesheetEntriesPlugin, IssuesEstimatesPlugin ]
 def get_traffic_plugins(business):
     return [ x(business) for x in traffic_plugin_classes ]
