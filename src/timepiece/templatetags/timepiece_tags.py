b5e9b3dcf2c3ae51a183b8ab2f329758983c0033
Gareth Priede <gtp@implicitdesign.co.za>
2015-03-02 15:05:52 +0200

    bug fix in timepieage tags

--- src/timepiece/templatetags/timepiece_tags.py
+++ src/timepiece/templatetags/timepiece_tags.py
@@ -251,22 +251,21 @@ def running_progress_for_user_in_sprint(context, user, project):
                              ["Closed issue hours","0", "(estimated 0 hours)"],
                              ["Estimated remaining hours", "0"],
                              ["--"],
-                                  ["Total adhoc hours", "0"],
+                             ["Total adhoc hours", "0"],
                              ["--"],
-                                  ["Estimated velocity", "0"],
+                             ["Estimated velocity", "0"],
                              ["Actual velocity", "0"] ]
     else:
         data['message'] = 'V = %.2f' % project.new_stats['per_user'][user]['calculated_velocity']
-        data['tooltips'] = [ ["Total hours", "%.2f" % (project.new_stats['per_user'][user]['hours'] or 0)],
 
         data['tooltips'] = [ ["Total hours", "%.2f" % (project.new_stats['per_user'][user]['hours'] or 0)],
                              ["Total issue hours", "%.2f"%(project.new_stats['per_user'][user]['hours_real'] or 0), "(estimated %.2f hours)"%(project.new_stats['per_user'][user]['points_non_adhoc'] or 0)],
                              ["Closed issue hours","%.2f"%(project.new_stats['per_user'][user]['hours_closed_real'] or 0), "(estimated %.2f hours)"%(project.new_stats['per_user'][user]['points_closed_non_adhoc'] or 0)],
                              ["Estimated remaining hours", "%.2f" % (project.new_stats['per_user'][user]['points_open_non_adhoc'] or 0)],
                              ["--"],
-                         ["Total adhoc hours", "%.2f" % (project.new_stats['per_user'][user]['hours_adhoc'] or 0)],
+                             ["Total adhoc hours", "%.2f" % (project.new_stats['per_user'][user]['hours_adhoc'] or 0)],
                              ["--"],
-                         ["Estimated velocity", "%.2f" % (project.new_stats['per_user'][user]['rate'].velocity or 0)],
+                             ["Estimated velocity", "%.2f" % (project.new_stats['per_user'][user]['rate'].velocity or 0)],
                              ["Actual velocity", "%.2f" % (project.new_stats['per_user'][user]['calculated_velocity'] or 0)] ]
                              
     return data
