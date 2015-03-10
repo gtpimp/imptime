41340417d146ce773d105bd9d871cafd9cc8ab20
Gareth Priede <gtp@implicitdesign.co.za>
2015-03-10 20:35:43 +0200

    quick clocker styling for active clock in entry

--- src/timepiece/views.py
+++ src/timepiece/views.py
@@ -5542,8 +5542,11 @@ def quick_clocker(request, template="timepiece/time-sheet/quick_clocker.html", c
     context['clocked_in_entries'] = timepiece.Entry.objects.all().filter(source='quick_clocker').is_open().order_by("user__username")
     context['recently_clocked_out_entries'] = timepiece.Entry.objects.all().filter(source='quick_clocker').is_closed().order_by("-date_updated")[0:15]
     context['recent_dev_entries'] = timepiece.Entry.objects.all().filter(source='emacs').is_closed().order_by("-end_time")[0:20]
+    logged_in_users_active_entry = context['clocked_in_entries'].filter(user=request.user).first()
     if context.get('clock_out_form', None) is None:
-        context['clock_out_form'] = timepiece_forms.QuickClockerClockOutForm(context['clocked_in_entries'], initial={'clock_out_time':datetime.datetime.now()})
+        context['clock_out_form'] = timepiece_forms.QuickClockerClockOutForm(context['clocked_in_entries'],
+                                                                             initial={'entry':logged_in_users_active_entry,
+                                                                                      'clock_out_time':datetime.datetime.now()})
     context['clock_in_form'] = clock_in_form
     
     return render_to_response(template, context, context_instance=RequestContext(request))
