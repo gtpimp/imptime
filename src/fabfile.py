1410d87583a7d347f8265d252e5c6e04eb20a64f
Charl <charl@implicitdesign.co.za>
2015-07-23 15:02:53 +0200

    Started making some changes to the fab file

--- src/fabfile.py
+++ src/fabfile.py
@@ -5,7 +5,7 @@ import shutil
 from fabric.contrib.console import confirm, prompt
 import datetime
 
-local_code_dir = os.path.dirname(os.path.realpath(__file__))
+local_code_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)))
 imp_remote_code_dir = "/home/timesheet"
 imp_remote_scripts_dir = "/home/timesheet/scripts"
 imp_remote_venv_dir = imp_remote_code_dir + "/venv"
@@ -22,8 +22,13 @@ To deploy live to implicitdesign.co.za
 
   > fab host_imp deploy_prod
 
+To deploy to staging on dev.implicitdesign.co.za
+------------------------------------------------
+
+  > fab staging deploy_staging:origin,branch
+
 To re-import timesheets on implicitdesign.co.za
---------------------------------------------
+-----------------------------------------------
   
   > fab host_imp import_timesheet
 
@@ -36,6 +41,11 @@ def host_imp():
     env.user = "gtp"
     env.hosts = ['timesheet.implicitdesign.co.za']
 
+def staging():
+    env.user = "impd"
+    env.hosts = ['impd@dev.implicitdesign.co.za']
+    env.base_dir = '/home/imptime/'
+
 # ===== top level commands ======
 
 def deploy_prod():
@@ -46,3 +56,24 @@ def deploy_prod():
 def import_timesheet(users=''):
     with cd(imp_remote_scripts_dir):
         run("./import_timesheets.sh")
+
+def deploy_staging(git_origin, branch):
+    print("Deploying: ** %s ** to staging server: %s" % (branch, env.hosts[0]))
+
+    with cd(local_code_dir):
+        current_branch = local('git describe --contains --all HEAD', capture=True)
+        if current_branch != branch:
+            print 'Branch %s is not currently checked out' % branch
+            return
+        local('git push origin %s' % branch)
+    
+    print 'Deploying - ', env.base_dir
+    release(env.base_dir, git_origin, branch)
+
+def release(dirpath, git_origin, branch):
+    with cd(dirpath):
+        run('git reset --hard HEAD')
+        run('git fetch %s' % git_origin)
+        run('git checkout %s' % branch)
+        run('git pull %s %s' % (git_origin, branch))
+        run('scripts/deploy_local.sh')
