cd26550b19fd24197189363155e58f271316b2f1
Gareth Priede <gtp@implicitdesign.co.za>
2016-01-26 08:48:55 +0200

    styling fix for firefox

--- src/noui/views.py
+++ src/noui/views.py
@@ -176,19 +176,22 @@ def command_import(request, context=None):
         logger.exception(ex)
         raise
 
-@login_required
 def next_action(request, context=None):
     context = context or {}
-    try:
-        next_action = PostedAction.objects.all().filter(target_user=request.user, status='waiting').order_by("id").first()
-        #next_action = PostedAction.objects.all().filter(target_user=request.user).order_by("id").first()
-        if next_action:
-            context['action'] = next_action.model_to_dict(convert_json_fields_to_json=True)
+    if not request.user.id:
+        context['action'] = None
         context['status'] = 'ok'
-    except Exception, ex:
-        logger.exception(ex)
-        context['status'] = 'failed'
-        context['error_msg'] = str(ex)
+    else:
+        try:
+            next_action = PostedAction.objects.all().filter(target_user=request.user, status='waiting').order_by("id").first()
+            #next_action = PostedAction.objects.all().filter(target_user=request.user).order_by("id").first()
+            if next_action:
+                context['action'] = next_action.model_to_dict(convert_json_fields_to_json=True)
+            context['status'] = 'ok'
+        except Exception, ex:
+            logger.exception(ex)
+            context['status'] = 'failed'
+            context['error_msg'] = str(ex)
     return HttpResponse(json.dumps(context), content_type='application/json')
 
 @login_required
