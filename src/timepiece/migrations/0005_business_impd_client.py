4dc3c8f7a775094837893b5cc6be3d8b4352de15
Gareth Priede <gtp@implicitdesign.co.za>
2016-02-02 14:37:31 +0200

    added Client to the migration fixed client-business association in migrations

--- src/timepiece/migrations/0005_business_impd_client.py
+++ src/timepiece/migrations/0005_business_impd_client.py
@@ -11,6 +11,18 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Client',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255, blank=True)),
+                ('code', models.CharField(max_length=100, blank=True)),
+                ('email', models.EmailField(max_length=254)),
+                ('logo', models.FileField(null=True, upload_to=b'logos', blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+            ],
+        ),
         migrations.AddField(
             model_name='business',
             name='impd_client',
--- src/timepiece/migrations/0006_auto_20160201_1248.py
+++ src/timepiece/migrations/0006_auto_20160201_1248.py
@@ -5,13 +5,11 @@ from django.db import migrations, models
 from timepiece import models
 
 def forwards_func(apps, schema_editor):
-    clients = models.Client.objects.filter(code='impd')
-    if clients.count == 0:
-        Client.objects.create(name='Implicit Design', code='impd', email='gtp@impd.co.za')
-    else:
-        impd_client = clients.all()[:1].get()
-        models.Business.objects.filter(impd_client__isnull=True).update(impd_client=impd_client)
-
+    Client = apps.get_model('timepiece.Client')
+    Business = apps.get_model('timepiece.Business')
+    
+    impd_client = Client.objects.create(name='Implicit Design', code='impd', email='gtp@impd.co.za')
+    Business.objects.filter(impd_client__isnull=True).update(impd_client=impd_client)
 
 def reverse_func(apps, schema_editor):
     pass
