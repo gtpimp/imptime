895d42025855930158cecb69b92ff6932f74ae70
Gareth Priede <gtp@implicitdesign.co.za>
2016-06-16 20:44:51 +0200

    wip

--- react_build/containers/ProjectsPage.js
+++ react_build/containers/ProjectsPage.js
@@ -1,6 +1,6 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { ProjectTable } from '../components/ProjectTable'
+import ProjectTable from '../components/ProjectTable'
 
 class ProjectsPage extends Component {
 
--- scripts/react_build_and_watch.sh
+++ scripts/react_build_and_watch.sh
@@ -2,9 +2,6 @@
 
 cd "`dirname \"$0\"`/.."
 ROOT=`pwd`
-cd ${ROOT}
 
-echo "Starting webpack in watch mode"
 cd ${ROOT}/react_build/
-
 webpack --progress --colors --watch --output-path=${ROOT}/src/static_collected/js
