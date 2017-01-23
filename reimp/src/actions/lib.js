5dcb83038557a0cbdec70bab1fc531e99a22ece3
Gareth Priede <gtp@impd.co.za>
2017-01-23 22:36:43 +0200

    Added throttling to the url requests, so they don't flood the server when something is wrong

--- reimp/src/actions/lib.js
+++ reimp/src/actions/lib.js
@@ -1,8 +1,12 @@
 import fetch from 'isomorphic-fetch'
 import cookie from 'react-cookie'
 import isArray from 'lodash/isArray'
+import moment from 'moment'
+
+const throttles = throttles || {}
 
 export function impfetch(url, args) {
+
     url = "" + url
     args = args || {}
     if ( ! args.headers ) {
@@ -19,5 +23,33 @@ export function impfetch(url, args) {
         let param_payload = JSON.stringify(args.params)
         url += "?params=" + param_payload
     }
-    return fetch(url, args)
+
+    const throttle = throttles[url] || {}
+    const THROTTLE_HIT_PAUSE_SECONDS = 5
+    const now = moment()
+    const last_failure_was_x_milliseconds_ago = throttle.last_failure_at && now.diff(throttle.last_failure_at, 'milliseconds') || null
+    if ( throttle.running || (last_failure_was_x_milliseconds_ago && last_failure_was_x_milliseconds_ago < THROTTLE_HIT_PAUSE_SECONDS*1000) ) {
+        return new Promise(function(resolve, reject) {
+            // note we reject because we don't want to say anything about
+            // whether the existing throttle succeeded, and the caller should
+            // try again after checking if the data doesn't exist
+            setTimeout(function() { reject(); }, THROTTLE_HIT_PAUSE_SECONDS*1000)
+        })
+    }
+
+    // we continue to reference the main throttles object to help with multi-threading
+    throttles[url] = throttle
+    throttles[url].running = true
+    const res = fetch(url, args)
+    res.then(function(response) {
+
+        if ( ( (""+response.status)[0] == "4" ) || ( (""+response.status)[0] == "5" ) ) {
+            throttles[url].last_failure_at = moment()
+        } else {
+            throttles[url].last_failure_at = null
+        }
+        
+        throttles[url].running = false
+    })    
+    return res
 }
