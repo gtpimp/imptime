6e00b43ddbcab97bf1cbfc4017354b7e11a1dd8d
Gareth Priede <gtp@implicitdesign.co.za>
2017-01-25 10:16:42 +0200

    fixed for settings.py

--- src/implicitdesign/settings.py
+++ src/implicitdesign/settings.py
@@ -424,7 +424,7 @@ WEEKLY_HOURS_MAIL_RECIPIENT = ""
 # DON'T PUT ANY MORE SETTINGS AFTER THIS POINT, OTHERWISE local_settings.py CAN'T OVERRIDE THEM
 #
 #
-if os.path.exists(os.path.join(PROJECT_HOME,"external_config","django_local_settings.py")):
+if os.path.exists(os.path.join(PROJECT_HOME, "external_config","django_local_settings.py")):
     from external_config.django_local_settings import *
 
 if os.path.exists(os.path.join(PROJECT_HOME,"version_number.py")):
