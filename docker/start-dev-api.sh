e2ba5599d6aa31127df2b9783f343013ad00166b
Gareth Priede <gtp@impd.co.za>
2017-05-07 10:17:34 +0200

    run collect static during build, just in case

--- docker/start-dev-api.sh
+++ docker/start-dev-api.sh
@@ -6,4 +6,5 @@ cd /opt/imptime/api/
 python manage.py wait_for_flag db_migrate_complete
 python manage.py wait_for_flag rabbitmq_ready
 
+python manage.py collectstatic --noinput
 python manage.py runserver 0.0.0.0:8002
