b3ae798afb2f122d97381d4cc28eb5b7c168b92f
Gareth Priede <gtp@impd.co.za>
2017-09-04 08:09:40 +0200

    send email to the timesheet user when import fails

--- src/emacs_importer/views.py
+++ src/emacs_importer/views.py
@@ -65,6 +65,11 @@ def import_timesheet(request):
                 status['errors'].append(str(ex))            
 
             if len(status.get('errors', [])) > 0:
+                send_mail(subject="Errors importing timesheet for %s : %s" %(username, form.filename),
+                          message="\n".join(status['errors']),
+                          from_email="info@implicitdesign.co.za",
+                          recipient_list=mail_to,
+                          fail_silently=True)
                 raise Exception("\n".join(status['errors']))
 
             if len(status.get('infos', [])) > 0:
