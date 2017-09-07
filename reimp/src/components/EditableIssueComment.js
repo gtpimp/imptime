864cad43214587b312cc7bd414d746b026b2f24e
Andrew Powrie <andrew@impd.co.za>
2017-09-07 18:12:04 +0200

    wip: trying to get issue comments to refresh after a change

--- reimp/src/components/EditableIssueComment.js
+++ reimp/src/components/EditableIssueComment.js
@@ -40,36 +40,36 @@ class EditableIssueComment extends Component {
         return (
 
             <div>
-            { comment.id &&
-              <EditableProperty property_key={'issue_comment_'+issue_id+'_'+comment.id}
-                                initial_value={comment.comment}
-                                onChange={this.onChange}
-                                can_edit={can_edit}
-              >
-                <IssueCommentForm form={'issue_comment_form_'+issue_id+'_'+comment.id}
-                                  issue_id={issue_id} comment={comment}/>
-                <div className="text-component--readonly text-component--comment">{comment.comment}</div>
-                <div className="text-component--empty"></div>
-              </EditableProperty>
-            }
-
-            { ! comment.id &&
-              <div>
-                <EditableProperty property_key={'issue_comment_'+issue_id}
-                                  initial_value=''
+              { comment.id &&
+                <EditableProperty property_key={'issue_comment_'+issue_id+'_'+comment.id}
+                                  initial_value={comment.comment}
                                   onChange={this.onChange}
                                   can_edit={can_edit}
-                >
-                  <IssueCommentForm form={'issue_comment_form_'+issue_id} issue_id={issue_id} />
-                  <div className="text-component--readonly"></div>
-                  <div className="text-component--empty">
-                    <button>Create comment</button>
-                  </div>
+                    >
+                  <IssueCommentForm form={'issue_comment_form_'+issue_id+'_'+comment.id}
+                                    issue_id={issue_id} comment={comment}/>
+                  <div className="text-component--readonly text-component--comment">{comment.comment}</div>
+                  <div className="text-component--empty"></div>
                 </EditableProperty>
-              </div>
-            }
+              }
 
-            { comment.id && <button onClick={this.onDelete}>delete</button> }
+              { ! comment.id &&
+                <div>
+                  <EditableProperty property_key={'issue_comment_'+issue_id}
+                                    initial_value=''
+                                    onChange={this.onChange}
+                                    can_edit={can_edit}
+                    >
+                    <IssueCommentForm form={'issue_comment_form_'+issue_id} issue_id={issue_id} />
+                    <div className="text-component--readonly"></div>
+                    <div className="text-component--empty">
+                      <button>Create comment</button>
+                    </div>
+                  </EditableProperty>
+                </div>
+              }
+
+              { comment.id && <button onClick={this.onDelete}>delete</button> }
             </div>
         )
     }
