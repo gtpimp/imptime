d5c410e76161da15331685a00167ec80020f4f85
Gareth Priede <gtp@implicitdesign.co.za>
2017-09-13 19:59:59 +0200

    fix for project statement if user has no time

--- reimp/src/components/SprintBreakdown.js
+++ reimp/src/components/SprintBreakdown.js
@@ -111,7 +111,7 @@ class SprintBreakdown extends Component {
                   </th>
                   { map(project_statement.users_with_time,
                         function(user_id) {
-                            const time_for_user = project_statement.times_by_user[user_id]
+                            const time_for_user = project_statement.times_by_user[user_id] || { total_hours: 0, total_billable_cost: 0}
                             return (
                                 <th key={user_id}>
                                   <table width="100%">
