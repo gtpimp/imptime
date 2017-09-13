0c54e0268f7add3d5f2eefa95c7e4964cee4c0f9
Gareth Priede <gtp@implicitdesign.co.za>
2017-09-13 19:31:35 +0200

    show all split time entries during import instead of halting on the first one

--- src/emacs_importer/extract_for_timepiece.py
+++ src/emacs_importer/extract_for_timepiece.py
@@ -193,7 +193,8 @@ class Extractor(object):
         for clock in orgnode.getClocks():
 
             if clock['from'].day != clock['to'].day:
-                raise Exception("Clock entry spans more than one day, if this is real then split the entry. From=%s, To=%s" % (clock['from'], clock['to']))
+                self.status['errors'].append("Clock entry spans more than one day, if this is real then split the entry. From=%s, To=%s" % (clock['from'], clock['to']))
+                continue
             
             Entry.objects.create(user=timesheet_user,
                                  source='emacs',
