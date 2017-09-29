034ed08aeb01bc6fc81cf691c42a529efaeecec2
Gareth Priede <gtp@impd.co.za>
2017-09-29 09:31:25 +0200

    importer: prevent error emails failing silently
    importer: send top level exception emails to admin only

--- src/emacs_importer/views.py
+++ src/emacs_importer/views.py
@@ -71,14 +71,14 @@ def import_timesheet(request):
                           message="\n".join(status['errors']),
                           from_email="info@implicitdesign.co.za",
                           recipient_list=mail_to,
-                          fail_silently=True)
+                          fail_silently=False)
 
             elif len(status.get('infos', [])) > 0:
                 send_mail(subject="Warnings importing timesheet for %s : %s" %(username, form.filename),
                           message="\n".join(status['infos']),
                           from_email="info@implicitdesign.co.za",
                           recipient_list=mail_to,
-                          fail_silently=True)
+                          fail_silently=False)
             
             return HttpResponse(json.dumps({'status':status,
                                             'msg':"Single file import of %s complete." % (form.filename)}))
@@ -87,7 +87,7 @@ def import_timesheet(request):
             send_mail(subject="Problems importing timesheet for %s : %s" %(username, form.filename),
                       message=str(ex),
                       from_email="info@implicitdesign.co.za",
-                      recipient_list=mail_to,
+                      recipient_list=settings.EMACS_ADMIN_USER_EMAILS,
                       fail_silently=True)
             return HttpResponse(json.dumps({'status':'failed', 'msg': str(ex)}))
 
