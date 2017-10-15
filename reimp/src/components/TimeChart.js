7a60a3948e41a526772f1cce89441905b6a4e3ce
Gareth Priede <gtp@implicitdesign.co.za>
2017-10-16 00:40:29 +0200

    typo

--- reimp/src/components/TimeChart.js
+++ reimp/src/components/TimeChart.js
@@ -44,7 +44,7 @@ class TimeChartTooltip extends Component {
               { map(payload, (series) =>
                   (
                       <div key={series.dataKey+"_"+label+"_"+name} className="time_chart__tooltip_series">
-                        { series.dataKey == 'graph_y' &&
+                        { series.dataKey == 'graph_y' || series.dataKey == "daily_hours" &&
                           <div>{series.value} hours on {moment(label).format('dddd DD-MMM-YYYY')}</div>
                         }
                         { series.dataKey == 'sick_days' && series.value > 0 &&
