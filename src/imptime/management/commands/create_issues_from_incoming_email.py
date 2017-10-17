11bf69edfde155933389b6fbd2aec8cbec047fdc
Gareth Priede <gtp@implicitdesign.co.za>
2017-10-17 23:07:26 +0200

    work in progress on the imap sync command

--- src/imptime/management/commands/create_issues_from_incoming_email.py
+++ src/imptime/management/commands/create_issues_from_incoming_email.py
@@ -10,23 +10,23 @@ logger = logging.getLogger(__name__)
 
 class Command(BaseCommand):
 
-    def handle(self, redmine_business_name, timepiece_business_name, force=False, **kwargs):
-
-        self.connection = imaplib2.IMAP4_SSL(settings.ISSUE_INBOX_HOST)
-        self.connection.login(settings.ISSUE_INBOX_USER, settings.ISSUE_INBOX_PASSWORD)
-        self.connection.select(settings.ISSUE_INBOX_FOLDER)
-        
+    def handle(self, *args, **kwargs):
         self.thread = Thread(target=self.idle)
         self.thread.start()
  
-    def join(self):
-        self.thread.join()
- 
     def idle(self):
+        logger.info("Starting imap idle thread")
         try:
             while True:
+
+                logger.info("Creating connection to " + settings.ISSUE_INBOX_HOST)
+                self.connection = imaplib2.IMAP4_SSL(settings.ISSUE_INBOX_HOST)
+                self.connection.login(settings.ISSUE_INBOX_USER, settings.ISSUE_INBOX_PASSWORD)
+                self.connection.select(settings.ISSUE_INBOX_FOLDER)
+                logger.info("Connected")
+                
                 try:
-                    self.needsync = False
+                    import pdb; pdb.set_trace()
                     def callback(args):
                         self.handle_email(args)
                     self.connection.idle(callback=callback)
@@ -43,9 +43,13 @@ class Command(BaseCommand):
                 finally:
                     self.connection.close()
                     self.connection.logout()
+        except Exception, ex:
+            logger.exception(ex)
         finally:
+            logger.info("Logging out")
             self.connection.close()
             self.connection.logout()
+            logger.info("Leaving imap idle thread")
  
     def handle_email(self, args):
-        print "Got an event!"
+        print "Got an event!: %s" % args
