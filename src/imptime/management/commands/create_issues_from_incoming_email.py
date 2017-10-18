bdacea11647b14f7c2fbc9ca036134f968f8e0f6
Gareth Priede <gtp@implicitdesign.co.za>
2017-10-18 23:06:51 +0200

    added expunge back

--- src/imptime/management/commands/create_issues_from_incoming_email.py
+++ src/imptime/management/commands/create_issues_from_incoming_email.py
@@ -139,9 +139,8 @@ class Command(BaseCommand):
                               recipient_list=to_addresses,
                               fail_silently=False)
             finally:
-                #self.inbox.store(message_number, '+FLAGS', '\\Deleted')
-                #self.inbox.expunge()
-                pass
+                self.inbox.store(message_number, '+FLAGS', '\\Deleted')
+                self.inbox.expunge()
 
     def notify_issues_created(self, user, project, issues_created):
 
