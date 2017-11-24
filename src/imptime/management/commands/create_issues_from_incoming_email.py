6c5030f54e5efc0307ae410490b63970e5bebc82
Gareth Priede <gtp@impd.co.za>
2017-11-24 10:15:44 +0200

    fix for duplicate issues when creating from email

--- src/imptime/management/commands/create_issues_from_incoming_email.py
+++ src/imptime/management/commands/create_issues_from_incoming_email.py
@@ -263,19 +263,25 @@ class Command(BaseCommand):
         return user
 
     def create_issue(self, message, user, project, sprint, raw_issue):
-        issue, is_new = Issue.objects.get_or_create(project=sprint,
-                                                    subject=raw_issue['subject'],
-                                                    defaults={'auto_created_during_import':True,
-                                                              'adhoc':False,
-                                                              'status2':IssueStatus.objects.get_or_create(name='new', business=project)[0],
-                                                              'feature':raw_issue['feature'],
-                                                              'assigned_to':user,
-                                                              'number':Issue.get_next_issue_number(project),
-                                                              'description':raw_issue['description'][0:settings.ISSUE_INBOX_MAX_ISSUE_DESCRIPTION_LENGTH],
-                                                              'story_points':0,
-                                                              'created':message['time'],
-                                                              'modified':message['time']})
-        SprintIssueOrder.insert_at_the_end(issue)
+        try:
+            issue = Issue.objects.filter(project=sprint, subject=raw_issue['subject']).order_by("-order").first()
+            if issue is None:
+                issue = Issue.objects.create(project=sprint,
+                                             subject=raw_issue['subject'],
+                                             auto_created_during_import=True,
+                                             adhoc=False,
+                                             status2=IssueStatus.objects.get_or_create(name='new', business=project)[0],
+                                             feature=raw_issue['feature'],
+                                             assigned_to=user,
+                                             number=Issue.get_next_issue_number(project),
+                                             description=raw_issue['description'][0:settings.ISSUE_INBOX_MAX_ISSUE_DESCRIPTION_LENGTH],
+                                             story_points=0,
+                                             created=message['time'],
+                                             modified=message['time'])
+                SprintIssueOrder.insert_at_the_end(issue)
+                is_new = True
+            else:
+                is_new = False
 
         if not is_new:
             IssueComment.objects.create(issue=issue,
