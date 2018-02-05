a9476c5ee2742e900d96382a321b5d60090db28b
gary <gary@impd.co.za>
2018-02-05 11:11:59 +0200

    post-diff code clean-up

--- reimp/src/components/SprintReviewPanel.js
+++ reimp/src/components/SprintReviewPanel.js
@@ -43,7 +43,6 @@ class SprintReviewPanel extends Component {
 
         return (
             <div className="sprint-review-panel">
-              <p>#Sprint Review Panel#</p>
               <div className="sprint-review-panel__review_due_per_user">
                 { map(sprint_review_ids, function(sprint_review_id) {
                       return (
@@ -55,7 +54,6 @@ class SprintReviewPanel extends Component {
                 }
                 { can_edit && <EditableSprintReviewCycle sprint_review_id={null} sprint_id={sprint_id}/> }
               </div>
-              <p>#Sprint Review Panel End#</p>
             </div>
         )
     }    
