b61a6aa5c61863428f418f4e793a03c5235fce8d
gary <gary@impd.co.za>
2018-02-23 19:45:09 +0200

    Mien persisting, but code needs lots of clean up

--- reimp/src/actions/Settings.js
+++ reimp/src/actions/Settings.js
@@ -1,6 +1,7 @@
 import { MIEN_LIST } from './ItemListKeyRegistry'
 import { get } from 'lodash'
 import { updateHeaderList } from './Page'
+import cookie from 'react-cookie';
 
 export const UPDATE_SETTINGS = 'UPDATE_SETTINGS'
 export const SET_MIEN_BUTTON = 'SET_MIEN_BUTTON'
@@ -21,12 +22,17 @@ export function getSetting(state, name) {
     return (state.settings || {})[name]
 }
 
-export function getMien(state) {
-    return get(state.settings, "mien", null)
+export function getMien(state, page_key) {
+    if ( cookie.load("current_mien") ) {
+        return cookie.load("current_mien")
+    } else {
+        return get(state.settings, "mien", null)
+    }
 }
 
 export function setMien(mien) {
 
+    cookie.save("current_mien", mien, {path: "/"})
     return {
         type: SET_MIEN,
         mien: mien
