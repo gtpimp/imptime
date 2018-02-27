ef99f74297fa53ac3bb7a212cf903c17da13d0fb
gary <gary@impd.co.za>
2018-02-27 16:13:56 +0200

    project dropdown buttons working when on projects page

--- reimp/src/components/BreadcrumbMenu.js
+++ reimp/src/components/BreadcrumbMenu.js
@@ -1,17 +1,99 @@
 import React, {Component} from 'react'
 import '../sass/breadcrumb.css'
 import {connect} from 'react-redux'
+import {
+    PAGE_KEY__PROJECTS_PAGE
+} from '../actions/ItemListKeyRegistry'
+import {
+    get_selected_project_ids
+} from '../actions/Page'
+import { getProject } from '../actions/Projects'
+import {browserHistory} from 'react-router'
+import {
+    startCandidateProject
+} from '../actions/Projects.js'
 
 class BreadcrumbMenu extends Component {
 
+    constructor(props) {
+        super(props)
+        this.onNewProjectClick = this.onNewProjectClick.bind(this)
+        this.onDashboardClick = this.onDashboardClick.bind(this)
+        this.onSprintsClick = this.onSprintsClick.bind(this)
+        this.onRoadmapClick = this.onRoadmapClick.bind(this)
+        this.onGalleryClick = this.onGalleryClick.bind(this)
+        this.onWikiClick = this.onWikiClick.bind(this)
+    }
+
+    onNewProjectClick() {
+        const { dispatch } = this.props
+        dispatch(startCandidateProject())
+    }
+
+    onDashboardClick() {
+        const {project_id} = this.props
+        browserHistory.push('/projects/' + project_id + '/dashboard/');
+    }
+
+    onRoadmapClick() {
+        const {project_id} = this.props
+        browserHistory.push('/projects/' + project_id + '/roadmap/');
+    }
+
+    onGalleryClick() {
+        const {project_id} = this.props
+        browserHistory.push('/projects/' + project_id + '/gallery/');
+    }
+
+    onWikiClick() {
+        const {project_id} = this.props
+        browserHistory.push('/projects/' + project_id + '/wiki/');
+    }
+
+    onSprintsClick() {
+        const {project_id} = this.props
+        browserHistory.push('/projects/' + project_id + '/sprints');
+    }
+
+
     render() {
+        const { project_id } = this.props
+        
         return (
             <div>
-              <span className="breadcrumb-menu__item">list item one</span>
-              <span className="breadcrumb-menu__item">list item two</span>
+              <div className="breadcrumb-menu__item" onClick={this.onNewProjectClick}>
+                + New Project
+              </div>
+              { project_id && 
+                <div className="breadcrumb-menu__item" onClick={this.onSprintsClick}>
+                  Sprints
+                </div>
+              }
+              { project_id && 
+                <div className="breadcrumb-menu__item" onClick={this.onDashboardClick}>
+                  Dashboard
+                </div>
+              }
+              { project_id && 
+                <div className="breadcrumb-menu__item" onClick={this.onRoadmapClick}>
+                  Roadmap
+                </div>
+              }
+              { project_id &&
+                <div className="breadcrumb-menu__item" onClick={this.onGalleryClick}>
+                  Gallery
+                </div>
+              }
+              { project_id &&
+                <div className="breadcrumb-menu__item" onClick={this.onWikiClick}>
+                  Wiki
+                </div>
+              }
+              
             </div>
         )
     }
+
 }
 
 function mapStateToProps(state, props) {
@@ -20,8 +102,14 @@ function mapStateToProps(state, props) {
     const project_re = /(\/projects\/\d+)$/
     const sprint_re = /(\/sprints\/\d+)$/
 
+    const selected_project_ids = get_selected_project_ids(state, PAGE_KEY__PROJECTS_PAGE)
+    const project = (selected_project_ids && selected_project_ids.length > 0 && getProject(state, selected_project_ids[0])) || {}
+    
     return {
+        selected_project_ids: selected_project_ids,
+        project_id: project.id
     }
+
 }
 
 export default connect(mapStateToProps)(BreadcrumbMenu)
