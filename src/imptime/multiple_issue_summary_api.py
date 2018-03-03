13852d303d60b2f81216576ac65305162c803e22
Gareth Priede <gtp@implicitdesign.co.za>
2018-03-03 22:06:38 +0200

    wip

--- src/imptime/multiple_issue_summary_api.py
+++ src/imptime/multiple_issue_summary_api.py
@@ -44,6 +44,7 @@ class MultipleIssueSummaryViewSet(BaseViewSet):
             res['all_tag_ids'] = [x for x in Tag.objects.filter(issues__in=qs).order_by("id").values_list("id", flat=True).distinct() if x]
 
             res['estimates_by_user'] = self._get_estimates_by_user(qs)
+            #res['estimates_by_tag_category'] = self._get_estimates_by_tag_category(qs)
             res['id'] = summary_id
             
             context['items'] = [ res ]
@@ -75,13 +76,13 @@ class MultipleIssueSummaryViewSet(BaseViewSet):
 
     def _get_estimates_by_user(self, issues_qs):
         estimates = {}
-        points = IssuePoints.objects.filter(issue__in=issues_qs)
+        points = IssuePoints.objects.filter(issue__in=issues_qs, points__gt=0)
         raw_estimated_hours = points.order_by("user_id").values("user_id").annotate(sum_points=Sum("points"))
 
         for x in raw_estimated_hours:
             estimates.setdefault(x['user_id'], {})['raw_estimates'] = x['sum_points']
 
-        estimates_with_rates=points.filter(points__gt=0, user__rates__project=F('issue__project'))\
+        estimates_with_rates=points.filter(user__rates__project=F('issue__project'))\
                                    .values('user_id', 'user__rates__velocity')\
                                    .order_by('user_id')
 
@@ -107,5 +108,12 @@ class MultipleIssueSummaryViewSet(BaseViewSet):
             for x in cost_with_commission:
                 estimates.setdefault(x['user_id'], {})['velocity_commission_cost'] = x['velocity_adjusted_cost']
 
+        return estimates
+    
+    def _get_estimates_by_tag_category(self, issues_qs):
+        estimates = {}
+        points = IssuePoints.objects.filter(issue__in=issues_qs, points__gt=0)
+        raw_estimated_hours = points.order_by("user_id", "issue__tags__category_id").values("user_id", "issue__tags__category_id").annotate(sum_points=Sum("points"))
+        
         return estimates
     
