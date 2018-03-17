9729f12344e43e08b268df22c7f0391a7cc29952
Gareth Priede <gtp@implicitdesign.co.za>
2018-03-17 18:10:43 +0200

    remove duplicates from async on the python side

--- src/impasync/middleware.py
+++ src/impasync/middleware.py
@@ -1,5 +1,7 @@
 from threading import local
 from channels import Group
+import copy
+from collections import OrderedDict
 import json
 from impasync.refresh_consumer import REFRESH_GROUP_NAME
 import logging
@@ -24,17 +26,20 @@ class MergeAsyncNotificationsMiddleware(object):
         self.post_notifications()
         return response
 
-    # def merge_duplicate_notifications(self, notifications):
-    #     merged_notifications = OrderedDict()
-    #     for notification in notifications:
-    #         entity_key = (notification['entity_name'], notification['entity_ref'])
-    #         merged_notifications[entity_key] = notification
-    #     return merged_notifications.values()
+    def merge_duplicate_notifications(self, notifications):
+        merged_notifications = OrderedDict()
+        for notification in notifications:
+            n_copy = copy.deepcopy(notification)
+            del n_copy['created_at']
+            del n_copy['modified_at']
+            key = json.dumps(n_copy, sort_keys=True)
+            merged_notifications[key] = notification
+        return merged_notifications.values()
 
     def post_notifications(self, notifications=None):
         if notifications is None:
             notifications = _active.notifications
             delattr(_active, 'notifications')
-            #notifications = self.merge_duplicate_notifications(notifications)
+            notifications = self.merge_duplicate_notifications(notifications)
         if notifications:
             Group(REFRESH_GROUP_NAME).send({"text":json.dumps(notifications)})
