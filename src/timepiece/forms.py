9349cf523ae70358e04e6820ac13d927e4c4bb5d
Gareth Priede <gtp@impd.co.za>
2018-03-26 10:53:34 +0200

    fix for generating quotes (adhoc error)

--- src/timepiece/forms.py
+++ src/timepiece/forms.py
@@ -1343,7 +1343,7 @@ class SprintQuoteReportSettingsForm(forms.Form):
 
         if only_these_issues is None:
             only_these_issues = project.issues.all()
-            only_these_issues = project.issues.filter(adhoc=False)
+            only_these_issues = project.issues.filter(issue_type='issue')
         self.fields['only_these_issue_numbers'].choices = [ (issue.number, issue.number) for issue in only_these_issues ]
         self.fields['only_these_issue_numbers'].initial = [ issue.number for issue in only_these_issues ]
 
