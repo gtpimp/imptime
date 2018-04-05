cdaa45caac1181da84efba72f7035f8bb8d24580
Gareth Priede <gtp@implicitdesign.co.za>
2018-04-05 19:56:55 +0200

    commented out two migrations which only have a purpose on production and rely on a model state which can't be guaranteed

--- src/timepiece/migrations/0102_assign_all_permission_to_gtp.py
+++ src/timepiece/migrations/0102_assign_all_permission_to_gtp.py
@@ -8,12 +8,7 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 def migrate(apps, schema_editor):
-
-    # We have to use real objects because I want to use the helpers on the timepiece models.
-    # This means this migration will become stale over time.
-    for user in User.objects.filter(username__in=['olivia', 'gtp', 'laila']):
-        for business in Business.objects.all():
-            BusinessPermissions.give_all_permissions_to_user(user=user, business=business)
+    return
 
 
 class Migration(migrations.Migration):
