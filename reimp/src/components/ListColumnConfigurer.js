b394be64e8f5c993608067eda306a85777399564
Gareth Priede <gtp@implicitdesign.co.za>
2018-05-04 21:54:19 +0200

    wip

--- reimp/src/components/ListColumnConfigurer.js
+++ reimp/src/components/ListColumnConfigurer.js
@@ -1,7 +1,10 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
+import classNames from 'classnames'
 import { keys, map, includes } from 'lodash'
 import { getMienBeingConfigured } from '../actions/Mien'
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
+
 
 class ListColumnConfigurer extends Component {
 
@@ -16,25 +19,58 @@ class ListColumnConfigurer extends Component {
          * dispatch(updateMienIssueHeaders(mien_being_configured.id, issue_headers))*/
     }
 
+    onDragEnd(result) {
+        /* const { onReorder } = this.props
+         * if (!result.destination) {
+         *     return;
+         * }
+         * if ( ! onReorder ) {
+         *     return;
+         * }
+         * const index_of_row_being_moved = result.source.index
+         * const index_of_destination = result.destination.index
+         * onReorder(index_of_row_being_moved, index_of_destination)*/
+    }
+
     render() {
-        const { mien, all_headers, active_headers } = this.props
-        const that = this
+        const { mien, all_headers } = this.props
         return (
             <div>
               <h3>Configuring issue list for {mien.title}</h3>
               <div className="list_header_configurer">
-                { map(keys(all_headers), function(header_key) {
-                      const header = all_headers[header_key]
-                      const label = header.description || header.label || header_key.replace(/_/g, " ")
-                      const is_enabled = includes(keys(mien.issue_headers), header_key)
-                      return (
-                          <div key={header_key}
-                               onClick={(event) => that.updateIssueHeaderConfiguration(event, header_key, !is_enabled)}>
-                            {label}
-                          </div>
-                      )
-                  })
-                }
+                <DragDropContext onDragEnd={this.onDragEnd}>
+                  <Droppable droppableId="droppable">
+                    {(provided, snapshot) => (
+                         <div ref={provided.innerRef}
+                              className={classNames({"div-table-wrapper--dragging":snapshot.isDragging})}
+                         >
+                           { map(keys(all_headers), function(header_key) {
+                                 const header = all_headers[header_key]
+                                 const label = header.description || header.label || header_key.replace(/_/g, " ")
+                                 // const is_enabled = includes(keys(mien.issue_headers), header_key)
+                                 return (
+                                     <Draggable key={header_key}
+                                                draggableId={header_key}>
+                                       {(provided, snapshot) => (
+                                            <div>
+                                              <div ref={provided.innerRef}
+                                                   className={classNames("list-column-configurer__row",
+                                                                         {"list-column-configurer__row--dragging":snapshot.isDragging})}
+                                                   style={{...provided.draggableStyle}}
+                                                   {...provided.dragHandleProps} >
+                                                {label}
+                                              </div>
+                                              {provided.placeholder}
+                                            </div>
+                                        )}
+                                     </Draggable>
+                                 )
+                             })
+                           }
+                         </div>
+                     )}
+                  </Droppable>
+                </DragDropContext>
               </div>
             </div>
         )
