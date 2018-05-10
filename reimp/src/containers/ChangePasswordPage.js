9e842d49b7f755988688484f5e59949b6d8714f9
Gareth Priede <gtp@implicitdesign.co.za>
2018-05-10 21:35:56 +0200

    final fix for inviting new users

--- reimp/src/containers/ChangePasswordPage.js
+++ reimp/src/containers/ChangePasswordPage.js
@@ -71,7 +71,7 @@ class ChangePasswordPage extends Component {
 function mapStateToProps(state, props) {
 
     let user = logged_in_user()
-    const has_usable_password = user.has_usable_password !== false && user.has_usable_password !== "false"
+    const has_usable_password = user.has_usable_password
     const user_id = user.user_id
     if ( user_id ) {
         user = Object.assign({}, user, getUser(state, user_id))
