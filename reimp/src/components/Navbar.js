8b224db3b094a133211d9786df19c880200028d7
Gareth Priede <gtp@implicitdesign.co.za>
2018-05-21 22:53:52 +0200

    typo

--- reimp/src/components/Navbar.js
+++ reimp/src/components/Navbar.js
@@ -72,7 +72,7 @@ class Navbar extends Component {
               </div>
               <div className="navbar__right">
                 { false && <div className="navbar__tab"><NavTab to="/nudge" label="Nudge" /></div> }
-                <div className="navbar__tab"><NavTab to='/schedule'/></div>
+                <div className="navbar__tab"><NavTab to='/schedule' label="Scheduler"/></div>
                 <div className="navbar__tab" onMouseOver={this.showCompanyMenu} onMouseLeave={this.hideCompanyMenu}>
                   <NavTab variant="dashboard-toggle" label="Company" />
                   { company_menu_visible &&
