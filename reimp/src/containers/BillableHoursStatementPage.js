646d42d5fc8727756a0b68cf1c29fbd698f17d18
Gareth Priede <gtp@implicitdesign.co.za>
2018-05-27 12:43:23 +0200

    className typo

--- reimp/src/containers/BillableHoursStatementPage.js
+++ reimp/src/containers/BillableHoursStatementPage.js
@@ -7,7 +7,7 @@ class BillableHoursStatementPage extends Component {
 
     render() {
         return (
-            <div class="billable-statement-page">
+            <div className="billable-statement-page">
               <h2>
                 Statement of billable hours across projects you have financial access to
               </h2>
