b94d8d287b70f6f778b3b675461f800cbc07a904
gary <gary@impd.co.za>
2018-06-20 10:31:48 +0200

    glamorous up and running. error on import breadcrumb.css so commented out imports

--- reimp/src/components/Breadcrumb.js
+++ reimp/src/components/Breadcrumb.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {Link, withRouter} from 'react-router-dom'
-import '../sass/breadcrumb.css'
+// import '../sass/breadcrumb.css'
 import { map, get, filter } from 'lodash'
 import { startCandidateProject } from '../actions/Projects'
 import { startCandidateSprint } from '../actions/Sprints'
