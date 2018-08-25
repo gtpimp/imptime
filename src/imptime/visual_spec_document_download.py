48fd2f91ef0ad8e111c754e41874b42fa72d2029
Gareth Priede <gtp@implicitdesign.co.za>
2018-08-25 12:35:31 +0200

    fix for downloading docs

--- src/imptime/visual_spec_document_download.py
+++ src/imptime/visual_spec_document_download.py
@@ -21,8 +21,9 @@ class VisualSpecDocumentBase(APIView):
         # jump through hoops because we want to download from a url
         # but the login token is normally passed in a custom header.
         user = get_user_by_token(request)
-        visual_spec_document = VisualSpecDocument.objects.filter(visual_spec_projects__project__in=PermissionHelper.allowed_projects(user))\
-                                                         .get(pk=visual_spec_document_id)
+        visual_spec_document = VisualSpecDocument.objects.filter(visual_spec_projects__project__in=PermissionHelper.allowed_projects(user),
+                                                                 pk=visual_spec_document_id)\
+                                                         .order_by("-id").first()
         bp = ProjectPermissions.for_user(user, visual_spec_document.visual_spec_projects.all()[0].project)
         if not bp.has_view_issues:
             return None
