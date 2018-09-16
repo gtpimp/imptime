095c8be7e26e26e99a4f0889900bc0f349d0f4db
Gareth Priede <gtp@implicitdesign.co.za>
2018-09-16 20:28:53 +0200

    added sorting (not wired in yet)

--- reimp/src/components/CommonTable.js
+++ reimp/src/components/CommonTable.js
@@ -1,14 +1,82 @@
 import React, {Component} from 'react'
 import { size, map } from 'lodash'
-import { AutoSizer, Table, Column } from 'react-virtualized'
+import Draggable from 'react-draggable'
+import { AutoSizer, defaultTableHeaderRenderer, defaultTableRowRenderer, Column, Table } from 'react-virtualized'
+import { SortableContainer, SortableElement, SortableHandle } from 'react-sortable-hoc'
 import MienListColumnConfigurable from './MienListColumnConfigurable'
 import 'react-virtualized/styles.css';
 
+const SortableTable = SortableContainer(Table, {
+    withRef: true
+})
+const SortableRow = SortableElement(defaultTableRowRenderer)
+const DragHandle = SortableHandle(({ label }) => (
+    <div>{label}</div>
+))
+
+const MIN_COLUMN_WIDTH = 50
+
+
 class CommonTable extends Component {
+
+    onRowSorted = () => {
+        window.alert("sorted")
+    }
+
+    rowRenderer = (props) => {
+        const { index } = props
+
+        return this.isRowSortable(index)
+             ? <SortableRow index={-1} {...props} />
+             : defaultTableRowRenderer(props)
+    }
+
+    resizeColumn = ({ dataKey, deltaX }) => {
+        window.alert("resized")
+    }
+
+    isRowSortable = (index) => {
+        return index >= 0 // Header row should not be draggable
+    }
+
+    renderDraggableColumn = (args) => {
+        const { rowIndex } = args
+        const { renderCell } = this.props
+        const content = renderCell(args)
+        if (this.isRowSortable(rowIndex)) {
+            return <DragHandle label={content} />
+        } else {
+            return content
+        }
+    }
+
+    draggableHeaderRenderer = (props) => {
+        return (
+            <div className='DraggableHeader'>
+              {defaultTableHeaderRenderer(props)}
+              <Draggable
+                  axis='x'
+                  defaultClassName='DragHandle'
+                  defaultClassNameDragging='DragHandleActive'
+                  onStop={(event, data) => this.resizeColumn({
+                          dataKey: props.dataKey,
+                          deltaX: data.x
+                      })}
+                  position={{
+                      x: 0,
+                      y: 0
+                  }}
+                  zIndex={999}
+              >
+                <div>||</div>
+              </Draggable>
+            </div>
+        )
+    }    
 
     render() {
         const { getAvailableHeaders, getHeaderListForMien, updateMienHeaders, header_list_name,
-                items, header_list, renderCell } = this.props
+                items, header_list } = this.props
 
         return (
 
@@ -21,22 +89,25 @@ class CommonTable extends Component {
 
                 <AutoSizer disableHeight>
                   {({width}) => (
-                       <Table height={500}
-                              headerHeight={20}
-                              rowCount={size(items)}
-                              rowHeight={30}
-                              width={width}
-                              rowGetter={({ index }) => items[index]}
+                       <SortableTable height={500}
+                                      headerHeight={20}
+                                      rowCount={size(items)}
+                                      onSortEnd={this.onRowSorted}
+                                      rowHeight={30}
+                                      width={width}
+                                      useDragHandle
+                                      rowRenderer={this.rowRenderer}
+                                      rowGetter={({ index }) => items[index]}
                        >
                          { map(header_list, (header) =>
                              <Column key={header.key}
                                      label={header.label}
                                      dataKey={header.key}
-                                     cellRenderer={renderCell}
+                                     cellRenderer={this.renderDraggableColumn}
                                      flexGrow={1}
                                      width={100} />
                            )}
-                       </Table>
+                       </SortableTable>
                    )}
                 </AutoSizer>
                 
@@ -45,6 +116,7 @@ class CommonTable extends Component {
         )        
         
     }
+    
     
 }
 
