e497b3bfd8b05409cfa6bf915e8f02cac70e81fe
Gareth Priede <gtp@impd.co.za>
2018-10-25 10:54:09 +0200

    wokring on cookies for puppeteer

--- src/puppeteer/impd_puppeteer.js
+++ src/puppeteer/impd_puppeteer.js
@@ -6,23 +6,27 @@ const puppeteer = require("puppeteer");
 
 cli.version("1")
     .option("--path <path>", "The output filepath")
+    .option("--auth_token <path>", "The authentication token for this user")
     .action(function(required, optional) {})
     .parse(process.argv);
 
 
 (async () => {
     let options = {};
-
-    _.each(cli.options, function(option) {
-        const option_name = option.name()
-        _.set(options, option_name, cli[option_name]);
-        console.log(option_name)
-    });
+    options["path"] = cli["path"]
+    
+    // console.log(options)
     
     const browser = await puppeteer.launch({ args: ["--no-sandbox"] });
     const page = await browser.newPage();
 
-    
+    page.setCookie({"name": "token",
+                    "value": "84fb0bcf9c24e3396fb1ff13e14c3f936394a36e",
+                    "domain": "localhost"})
+    page.setCookie({"name": "user_id",
+                    "value": "3",
+                    "domain": "localhost"})
+
     const location = _.first(cli.args);
     await page.goto(location, {
         waitUntil: _.get(options, "waitUntil", "networkidle2")
