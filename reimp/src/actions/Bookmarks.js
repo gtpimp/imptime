e442270852716751e23a7510e3ed3b68505d04fb
Gareth Priede <gtp@implicitdesign.co.za>
2018-11-01 20:13:19 +0200

    if the user clicks on a breadcrumb in the auto-bookmark, don't replace the bookmark with this shorter version

--- reimp/src/actions/Bookmarks.js
+++ reimp/src/actions/Bookmarks.js
@@ -1,5 +1,5 @@
 import cookie from 'react-cookies'
-import { first, map, compact, filter, find } from 'lodash'
+import { size, first, map, compact, filter, find } from 'lodash'
 import { getRootEntityBreadcrumb, getLeafEntityBreadcrumb, getBreadcrumbs } from './Breadcrumbs'
 
 const KEY = 'bookmarks'
@@ -33,6 +33,21 @@ function createBookmarkFromBreadcrumbs(breadcrumbs) {
     return bookmark
 }
 
+function doesBookmarkASubsumeBookmarkB(bookmark_a, bookmark_b) {
+    if ( size(bookmark_a.parts) <= size(bookmark_b.parts) ) {
+        return false
+    }
+    let subsumes = true
+    map(bookmark_b.parts, (part_b, index) => {
+        const part_a = bookmark_b.parts[index]
+        if (part_a.url != part_b.url) {
+            subsumes = false
+            return
+        }
+    })
+    return subsumes
+}
+
 export function autoBookmarkCurrent(optional_breadcrumbs) {
     return (dispatch, getState) => {
         const state = getState()
@@ -47,7 +62,12 @@ export function autoBookmarkCurrent(optional_breadcrumbs) {
         if ( existing_auto_bookmark ) {
             existing_auto_bookmarks = filter(existing_auto_bookmarks, (x) => x.id !== existing_auto_bookmark.id)
         }
-        existing_auto_bookmarks.unshift(new_auto_bookmark)
+
+        if ( doesBookmarkASubsumeBookmarkB(existing_auto_bookmark, new_auto_bookmark) ) {
+            existing_auto_bookmarks.unshift(existing_auto_bookmark)
+        } else {
+            existing_auto_bookmarks.unshift(new_auto_bookmark)
+        }
         saveAutoBookmarks(existing_auto_bookmarks)
     }
 }
