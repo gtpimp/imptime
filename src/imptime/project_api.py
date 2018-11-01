047e90f7e8897f0312da621ddf1d2cfce2b2fe5b
Gareth Priede <gtp@implicitdesign.co.za>
2018-11-01 20:06:27 +0200

    disabled bcc on project invite, it's too much of a security risk

--- src/imptime/project_api.py
+++ src/imptime/project_api.py
@@ -201,9 +201,7 @@ class ProjectViewSet(BaseViewSet):
                     from_address=settings.FROM_EMAIL,
                     text_content=plain_content,
                     html_content=html_content,
-                    to_addresses=[invite_user.email],
-                    bcc_addresses=[v for k,v in settings.CUSTOMER_SERVICE_EMAILS])
-
+                    to_addresses=[invite_user.email]) 
     def delete(self, request, pk):
         try:
             params = request.data
