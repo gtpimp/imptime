e0a02b0c2e8efbe1495601886d25315ab8457382
Gareth Priede <gtp@implicitdesign.co.za>
2019-01-19 20:59:35 +0200

    refactoring testable lines

---
+++ reimp/src/actions/TestableLines.js
@@ -0,0 +1,73 @@
+import { ENTITY_KEY__TESTABLE_LINE } from '../actions/ItemListKeyRegistry'
+
+import {
+    invalidateAllItems,
+    invalidateItems,
+    fetchItemsIfNeeded,
+    ensureItemsLoaded,
+    getItem,
+    getItems,
+    getItemsById,
+    updateItem,
+    startCandidateItem,
+    saveCandidateItem,
+    deleteItems,
+} from '../actions/Item'
+
+export function invalidateAllTestableLines() {
+    return (dispatch, getState) => {
+        dispatch(invalidateAllItems(ENTITY_KEY__TESTABLE_LINE))
+    }
+}
+
+export function invalidateTestableLines(testable_line_ids_to_invalidate) {
+    return (dispatch, getState) => {
+        dispatch(invalidateItems(ENTITY_KEY__TESTABLE_LINE,
+                                 testable_line_ids_to_invalidate
+        ))
+    }
+}
+
+export function updateTestableLine(testable_line_ids, instruction, on_done) {
+    return updateItem(ENTITY_KEY__TESTABLE_LINE, testable_line_ids, "instruction", instruction, on_done)
+}
+
+export function fetchTestableLinesIfNeeded(list_key) {
+    return (dispatch, getState) => {
+        dispatch(fetchItemsIfNeeded(ENTITY_KEY__TESTABLE_LINE, list_key))
+    }
+}
+
+export function ensureTestableLinesLoaded(testable_line_ids) {
+    return ensureItemsLoaded(ENTITY_KEY__TESTABLE_LINE, testable_line_ids)
+}
+
+export function getTestableLine(state, testable_line_id) {
+    return getItem(state, ENTITY_KEY__TESTABLE_LINE, testable_line_id)
+}
+
+export function getTestableLines(state, testable_line_ids) {
+    return getItems(state, ENTITY_KEY__TESTABLE_LINE, testable_line_ids) || {}
+}
+
+export function getTestableLinesById(state, testable_line_ids) {
+    return getItemsById(state, ENTITY_KEY__TESTABLE_LINE, testable_line_ids)
+}
+
+export function createTestableLine(testable_id, instruction) {
+    return (dispatch, getState) => {
+        dispatch(startCandidateItem(ENTITY_KEY__TESTABLE_LINE, instruction))
+        dispatch(updateCandidateDetails(ENTITY_KEY__TESTABLE_LINE, {testable_id: testable_id}))
+        dispatch(saveCandidateItem(ENTITY_KEY__TESTABLE_LINE))
+    }
+}
+
+export function deleteTestableLine(testable_line_id) {
+    return (dispatch, getState) => {
+        dispatch(deleteItems(ENTITY_KEY__TESTABLE_LINE, [testable_line_id]))
+    }
+}
+
+export function is_testable_line_invalidated(state, testable_line_id) {
+    return is_item_invalidated(ENTITY_KEY__TESTABLE_LINE, state, testable_line_id)
+}
--- reimp/src/components/EditableTestableLine.js
+++ reimp/src/components/EditableTestableLine.js
@@ -3,22 +3,21 @@ import {connect} from 'react-redux'
 import map from 'lodash/map'
 import EditableProperty from './form/EditableProperty'
 import PermissionInspectorHighlighter from './PermissionInspectorHighlighter'
-import {
-    updateIssueTestable,
-    createIssueTestable,
-    deleteIssueTestable,
-    ensureIssuesLoaded,
-    getIssue,
-    is_issue_invalidated,
-    promoteIssueTestableToIssue
-} from '../actions/Issues'
-
 import SidebarAddButton from './SidebarAddButton'
 import TestableLineForm from './form/TestableLineForm'
 import { has_permission } from '../actions/Users'
 import TestableLine from './TestableLine'
+import Loading from './Loading'
+import {
+    getTestableLine,
+    ensureTestableLineLoaded,
+    updateTestableLine,
+    createTestableLine,
+    deleteTestableLine,
+    is_testable_invalidated
+} from '../actions/TestableLines'
 
-class EditableIssueTestable extends Component {
+class EditableTestableLine extends Component {
 
     constructor(props) {
         super(props)
@@ -27,49 +26,54 @@ class EditableIssueTestable extends Component {
     }
 
     componentWillMount() {
-        const { dispatch, issue_id } = this.props
-        dispatch(ensureIssuesLoaded([issue_id]))
+        const { dispatch, testable_line_id } = this.props
+        dispatch(ensureTestableLineLoaded([testable_line_id]))
     }
 
     componentWillReceiveProps(new_props) {
         const { dispatch } = this.props
-        const { issue_id } = new_props
-        dispatch(ensureIssuesLoaded([issue_id]))
+        const { testable_line_id } = new_props
+        dispatch(ensureTestableLineLoaded([testable_line_id]))
     }
 
     onChange(new_value) {
-        const { dispatch, issue_id, testable_line_id } = this.props
+        const { dispatch, testable_id, testable_line_id } = this.props
         if ( testable_line_id ) {
-            dispatch(updateIssueTestableLine(issue_id, testable_id, testable_line_id, new_value.testable_line))
+            dispatch(updateTestableLine(testable_line_id, new_value.testable_line))
         } else {
-            dispatch(createIssueTestableLine(issue_id, testable_id, new_value.testable_line))
+            dispatch(createTestableLine(testable_id, new_value.testable_line))
         }
     }
 
     onDelete(event) {
-        const { dispatch, issue_id, testable_id, testable_line_id } = this.props
+        const { dispatch, testable_line_id } = this.props
         event.stopPropagation()
         if (! window.confirm("Are you sure you want to delete this testable line?" ) ) {
             return false;
         }
-        dispatch(deleteIssueTestableLine(issue_id, testable_id, testable_line_id))
+        dispatch(deleteTestableLine(testable_line_id))
     }
 
     render()
     {
-        const {testable, testable_line, can_edit, issue_id, project_id} = this.props
+        const {testable_line, can_edit, project_id} = this.props
+
+        if ( ! testable_line || ! testable_line.id ) {
+            return <Loading/>
+        }
+        
         return (
 
             <PermissionInspectorHighlighter project_id={project_id}
                                             permission_name='has_edit_description'>
               { testable_line.id &&
-                <EditableProperty property_key={'issue_testable_'+issue_id+'_'+testable.id}
-                                  initial_value={testable}
+                <EditableProperty property_key={'issue_testable_line_'+testable_line.id}
+                                  initial_value={testable_line}
                                   onChange={this.onChange}
                                   can_edit={can_edit}
                 >
-                  <TestableLineForm form={'issue_testable_line_form_'+issue_id+'_'+testable.id+'_'+testable_line.id}
-                                testable={testable}/>
+                  <TestableLineForm form={'testable_line_form_'+testable_line.id}
+                                testable_line={testable_line}/>
                   <TestableLine testable_line={testable_line}
                                 onDelete={this.onDelete}
                   />
@@ -80,7 +84,7 @@ class EditableIssueTestable extends Component {
               <div className="issue-testable__button-bar">
                 { ! testable_line.id &&
                   <div className="issue-testable__button-bar__container">
-                    <EditableProperty property_key={'issue_testable_'+issue_id}
+                    <EditableProperty property_key={'testable_line'+testable_line.id}
                                       initial_value=''
                                       onChange={this.onChange}
                                       can_edit={can_edit}
@@ -104,35 +108,17 @@ class EditableIssueTestable extends Component {
 
 function mapStateToProps(state, props) {
 
-    const { issue_id, testable_id, testable_line_id } = props
-    const issue = getIssue(state, issue_id) || {}
-    const can_edit = has_permission(state, issue.project_id, 'has_edit_description')
-
+    const { project_id, testable_id, testable_line_id } = props
+    const can_edit = has_permission(state, project_id, 'has_edit_description')
+    const testable_line = getTestableLine(state, testable_line_id)
     let testable = { id: null}
-    map(issue.testables || [], function(issue_testable, index) {
-        if ( issue_testable.id === testable_id ) {
-            testable = issue_testable
-        }
-    })
-
-    let testable_line = { id: null }
-    map(get(testable, ["lines"], []), function(issue_testable_line, index) {
-        if ( issue_testable_line.id === testable_line_id ) {
-            testable_line = issue_testable_line
-        }
-    })
     
-
     return {
-        issue_id,
-        issue,
-        sprint_id: issue.sprint_id,
-        testable_id,
-        testable,
+        project_id,
+        testable_line_id,
         testable_line,
         can_edit,
-        project_id: issue.project_id,
-        is_invalidated: is_issue_invalidated(state, issue.id),
+        is_invalidated: is_testable_invalidated(state, testable_line_id),
     }
 }
 
