dd25add16dadd4d23a29027f4bb3d2c8be5248e9
dreyer <dreyer@impd.co.za>
2019-02-27 10:41:17 +0200

    refresh on cards page ensures project and sprint gets loaded

--- reimp/src/components/Cards.js
+++ reimp/src/components/Cards.js
@@ -1,10 +1,42 @@
 import React, {Component} from 'react'
 import { connect } from 'react-redux'
-import { Link } from 'react-router-dom'
+import { Link, withRouter } from 'react-router-dom'
 import { css } from 'emotion'
 import { default_theme as theme } from '../theme/default'
+import {ensureProjectsLoaded, getProject} from '../actions/Projects'
+import {ensureSprintsLoaded, getSprint} from '../actions/Sprints'
+import { setSprintBreadcrumbsHelper } from '../actions/Breadcrumbs'
 
 class Cards extends Component {
+
+    constructor(props) {
+        super(props)
+    }
+
+    componentDidMount() {
+        const {sprint_id, sprint, project_id, project, dispatch} = this.props
+        dispatch(ensureProjectsLoaded([project_id]))
+        dispatch(ensureSprintsLoaded([sprint_id]))
+        this.refresh(sprint, project)
+    }
+
+    componentWillReceiveProps(new_props) {
+        const { sprint_id, project_id, dispatch } = this.props
+        dispatch(ensureProjectsLoaded([project_id]))
+        dispatch(ensureSprintsLoaded([sprint_id]))
+
+        if ( new_props.sprint.id !== this.props.sprint.id ||
+             new_props.sprint.name !== this.props.sprint.name ||
+             new_props.project.name !== this.props.project.name) {
+            this.refresh(new_props.sprint, new_props.project)
+        }
+    }
+
+    refresh(sprint, project) {
+        const { dispatch } = this.props
+        dispatch(setSprintBreadcrumbsHelper(project, sprint))
+    }
+
     render() {
         return (
             <div className={ main }>
@@ -25,7 +57,21 @@ class Cards extends Component {
     }
 }
 
-export default connect(null)(Cards)
+function mapStateToProps(state, props) {
+    const project_id = props.match.params.projectId
+    const sprint_id = props.match.params.sprintId
+    const sprint = getSprint(state, sprint_id) || {}
+    const project = getProject(state, project_id) || {}
+
+    return {
+        project_id,
+        sprint_id,
+        sprint,
+        project
+    }
+}
+
+export default withRouter(connect(mapStateToProps)(Cards))
 
 const main = css`
 display: flex;
