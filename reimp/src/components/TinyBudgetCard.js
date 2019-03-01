d1291539c2dd9565053ab5618af01362dabef2d9
dreyer <dreyer@impd.co.za>
2019-03-01 16:37:04 +0200

    adds permission constraints on budget card

--- reimp/src/components/TinyBudgetCard.js
+++ reimp/src/components/TinyBudgetCard.js
@@ -40,13 +40,13 @@ class BudgetCard extends Component {
     render() {
         const { budget, sprint_name, project_name, sprint_id, project_id } = this.props
         return (
-            <PermissionInspectorHighlighter project_id={project_id} permission_name="has_edit_budget">
-              <PermissionInspectorHighlighter project_id={project_id} permission_name="has_view_budget">
-                <TinyCard title="Budget" project_name={project_name} sprint_name={sprint_name}>
+            <TinyCard title="Budget" project_name={project_name} sprint_name={sprint_name}>
+              <PermissionInspectorHighlighter project_id={project_id} permission_name="has_edit_budget">
+                <PermissionInspectorHighlighter project_id={project_id} permission_name="has_view_budget">
                   <EditableSprintBudget sprint_id={sprint_id} />
-                </TinyCard>
+                </PermissionInspectorHighlighter>
               </PermissionInspectorHighlighter>
-            </PermissionInspectorHighlighter>
+            </TinyCard>
         )
     }
 }
