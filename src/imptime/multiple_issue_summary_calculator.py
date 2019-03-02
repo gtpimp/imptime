60e22e591130298c430ccd1bc723d3658f0c1bc6
Gareth Priede <gtp@implicitdesign.co.za>
2019-03-02 16:15:25 +0200

    all billable rates in proposal include commission and exclude scope creep

--- src/imptime/multiple_issue_summary_calculator.py
+++ src/imptime/multiple_issue_summary_calculator.py
@@ -64,6 +64,8 @@ class MultipleIssueSummaryCalculator(object):
                                    .values('user_id', 'user__rates__velocity')\
                                    .order_by('user_id')
 
+        commission_clause = 100/(100-F('user__rates__project__commission_percentage'))
+        
         velocity_adjusted_hours_by_user = estimates_with_rates.annotate(velocity_adjusted_points=Sum(F('user__rates__velocity')*F('points')))
 
         for x in velocity_adjusted_hours_by_user:
@@ -75,29 +77,12 @@ class MultipleIssueSummaryCalculator(object):
         if self.has_view_ctc_billable_rates:
 
             velocity_adjusted_costs = estimates_with_rates\
-                                      .annotate(velocity_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount'),
+                                      .annotate(velocity_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount')*commission_clause,
                                                                            output_field=FloatField()))
 
             for x in velocity_adjusted_costs:
                 if self.has_see_other_user_points or x['user_id'] == self.user.id:
                     estimates.setdefault(x['user_id'], {})['velocity_cost'] = x['velocity_adjusted_cost']
-
-            scope_creep_adjusted_costs = estimates_with_rates\
-                                         .annotate(scope_creep_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount')*F('issue__project__ratio_scope_creep'),
-                                                                                 output_field=FloatField()))
-
-            for x in scope_creep_adjusted_costs:
-                if self.has_see_other_user_points or x['user_id'] == self.user.id:
-                    estimates.setdefault(x['user_id'], {})['scope_creep_cost'] = x['scope_creep_adjusted_cost']                    
-
-            cost_with_commission = estimates_with_rates\
-                                   .annotate(scope_creep_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount')*F('issue__project__ratio_scope_creep')*100/
-                                                                        (100-F('user__rates__project__commission_percentage')),
-                                                                         output_field=FloatField()))
-
-            for x in cost_with_commission:
-                if self.has_see_other_user_points or x['user_id'] == self.user.id:
-                    estimates.setdefault(x['user_id'], {})['scope_creep_commission_cost'] = x['scope_creep_adjusted_cost']
 
         return estimates
 
@@ -109,28 +94,19 @@ class MultipleIssueSummaryCalculator(object):
                   'assigned_to__user_points__points',
                   'assigned_to__rates__velocity',
                   'velocity_adjusted_estimate',
-                  'scope_creep_adjusted_estimate'
         ]
         issues_qs = issues_qs.filter(assigned_to__rates__project=F('project'),
                                      assigned_to__user_points__issue_id=F('id'))\
                              .order_by("id")
         issues_qs = issues_qs.annotate(velocity_adjusted_estimate=Sum(F('assigned_to__rates__velocity')*F('assigned_to__user_points__points'),
                                                                       output_field=FloatField()))
-        issues_qs = issues_qs.annotate(scope_creep_adjusted_estimate=Sum(F('assigned_to__rates__velocity')*F('assigned_to__user_points__points')*F('project__ratio_scope_creep'),
-                                                                         output_field=FloatField()))
-        if self.has_view_ctc_billable_rates:
-            issues_qs = issues_qs.annotate(velocity_adjusted_cost=Sum(F('assigned_to__rates__velocity')*F('assigned_to__user_points__points')*F('assigned_to__rates__billable_amount')*100/
-                                                                      (100-F('assigned_to__rates__project__commission_percentage')),
-                                                                      output_field=FloatField()))
 
-            issues_qs = issues_qs.annotate(scope_creep_adjusted_cost=Sum(F('assigned_to__rates__velocity')*F('assigned_to__user_points__points')*F('assigned_to__rates__billable_amount')*F('project__ratio_scope_creep')*100/
-                                                                         (100-F('assigned_to__rates__project__commission_percentage')),
-                                                                         output_field=FloatField()))
-            
-            values.append('assigned_to__rates__billable_amount')
-            values.append('assigned_to__rates__project__commission_percentage')
+        commission_clause = 100/(100-F('assigned_to__rates__project__commission_percentage'))
+        
+        if self.has_view_ctc_billable_rates:
+            issues_qs = issues_qs.annotate(velocity_adjusted_cost=Sum(F('assigned_to__rates__velocity')*F('assigned_to__user_points__points')*F('assigned_to__rates__billable_amount')*commission_clause,
+                                                                      output_field=FloatField()))
             values.append('velocity_adjusted_cost')
-            values.append('scope_creep_adjusted_cost')
             
         issues_qs = issues_qs.values(*values)
 
@@ -174,8 +150,9 @@ class MultipleIssueSummaryCalculator(object):
                     estimates_by_tag_category[x['category_id']][x['user_id']][x['issue__tags__id']]['raw_estimates']
 
         if self.has_view_ctc_billable_rates:
+            commission_clause = 100/(100-F('user__rates__project__commission_percentage'))
             velocity_adjusted_costs = estimates_with_rates_by_tag\
-                                      .annotate(velocity_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount'),
+                                      .annotate(velocity_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount')*commission_clause,
                                                                            output_field=FloatField()),
                                                 category_id=F('issue__tags__category_id'))
             for x in velocity_adjusted_costs:
@@ -184,30 +161,19 @@ class MultipleIssueSummaryCalculator(object):
                                              .setdefault(x['user_id'], {})\
                                              .setdefault(x['issue__tags__id'], {})\
                                              ['velocity_cost'] = x['velocity_adjusted_cost']
-
-            costs_with_commission = estimates_with_rates_by_tag\
-                                    .annotate(velocity_adjusted_cost=Sum(F('user__rates__velocity')*F('points')*F('user__rates__billable_amount')*100/
-                                                                        (100-F('user__rates__project__commission_percentage')),
-                                                                         output_field=FloatField()),
-                                              category_id=F('issue__tags__category_id'))
-            for x in costs_with_commission:
-                if self.has_see_other_user_points or x['user_id'] == self.user.id:
-                    estimates_by_tag_category.setdefault(x['category_id'], {})\
-                                             .setdefault(x['user_id'], {})\
-                                             .setdefault(x['issue__tags__id'], {})\
-                                             ['velocity_commission_cost'] = x['velocity_adjusted_cost']
-                
             
         return estimates_by_tag_category
 
     def _get_actuals_enriched_with_costs(self, entries, include_rates=False):
+        commission_clause = 100/(100-F('user__rates__project__commission_percentage'))
         enriched = entries.annotate(sum_hours=Sum('hours'),
                                     cost=Sum(F('hours')*F('user__rates__billable_amount')),
-                                    cost_with_commission=Sum(F('hours')*F('user__rates__billable_amount')*F('user__rates__project__ratio_scope_creep')*100/(100-F('user__rates__project__commission_percentage')),
+                                    cost_with_commission=Sum(F('hours')*F('user__rates__billable_amount')*commission_clause,
                                                              output_field=FloatField()))
         if include_rates:
             # Note that this will separate entries by user, so you should only do this for '_by_user' type summaries
-            enriched = enriched.annotate(rate_with_commission=ExpressionWrapper(F('user__rates__billable_amount')*100/(100-F('user__rates__project__commission_percentage')), output_field=FloatField()))
+            enriched = enriched.annotate(rate_with_commission=ExpressionWrapper(F('user__rates__billable_amount')*commission_clause,
+                                                                                output_field=FloatField()))
                                 
         return enriched
     
