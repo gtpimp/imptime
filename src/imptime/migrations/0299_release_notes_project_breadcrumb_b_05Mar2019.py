33c3246a6af1eb3e889dd3098a3ffed00040acd8
Gareth Priede <gtp@implicitdesign.co.za>
2019-03-05 16:06:49 +0200

    release note

---
+++ src/imptime/migrations/0299_release_notes_project_breadcrumb_b_05Mar2019.py
@@ -0,0 +1,19 @@
+
+from __future__ import unicode_literals
+
+from django.db import migrations
+
+def forwards(apps, schema_editor):
+    ReleaseNote = apps.get_model('imptime', 'ReleaseNote')
+    ReleaseNote(header="project breadcrumb bug",
+                content="fixed bug where switching projectrs didn't update the breadcrumbs").save()
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('imptime', '0298_release_notes_company_page_fix_04Mar2019'),
+    ]
+
+    operations = [
+        migrations.RunPython(forwards, migrations.RunPython.noop),
+    ]
