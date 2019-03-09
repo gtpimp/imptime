4fb87a41129613bbca3a1a808339b213b2844571
Gareth Priede <gtp@implicitdesign.co.za>
2019-03-09 19:25:54 +0200

    wrote api for event_log_api

--- src/imptime/event_log_api.py
+++ src/imptime/event_log_api.py
@@ -17,7 +17,7 @@ from rest_framework.decorators import permission_classes
 from timepiece.models import Issue, IssueHistory, Entry, ProjectRole, Tag, IssuePoints, BusinessPermissions
 from timepiece.models import Business as Project
 from timepiece.models import Project as Sprint
-from imptime.event_log_serializer import EventLogFilterSerializer
+from imptime.event_log_serializer import EventLogFilterSerializer, EventLogSerializer
 
 logger = logging.getLogger(__name__)
 
@@ -32,11 +32,23 @@ class EventLogViewSet(BaseViewSet):
             params = request.GET.get('params', '{}')
             params = json.loads(params)
             pagination = params.get('pagination', {})
-            filter = params['additional_params']['filter']
             summary_id = params['filter']['ids'][0]
+            filter = self._get_filter(params)
 
-            #issue_qs = self.apply_filter(issue_qs, {}, issue_filter)
-            context['items'] = [ {'id':summary_id} ]
+            project = self.allowed_project(pk=filter['project_id'])
+            issues = Issue.objects.filter(project__business_id=project.id) #sic
+            issue_histories = IssueHistory.objects.filter(issue_id__in=issues,
+                                                          created_at__gte=filter['date_from_inclusive'],
+                                                          created_at__lte=filter['date_from_inclusive'])
+            clock_entries = Entry.objects.filter(issue_id__in=issues,
+                                                 end_time__gte=filter['date_from_inclusive'],
+                                                 start_time__lte=filter['date_to_inclusive'])
+
+            context['items'] = EventLogSerializer([{'id': summary_id,
+                                                    'issue_histories': issue_histories,
+                                                    'clock_entries': clock_entries}],
+                                                  many=True).data
+            
             context['pagination'] = pagination
             data = {'status': 'success', 'payload': context}
         except Exception, ex:
@@ -44,11 +56,7 @@ class EventLogViewSet(BaseViewSet):
             return self.error_response(ex)
         return HttpResponse(JSONRenderer().render(data))
 
-    def apply_filter(self, qs, raw_filter_args, issue_filter):
-        issue_ids = issue_filter.pop('issue_ids', None)
-        if issue_ids:
-            qs = qs.filter(pk__in=[x for x in issue_ids if x])
-        sprint_ids = issue_filter.pop('sprint_ids', None)
-        if sprint_ids:
-            qs = qs.filter(project_id__in=[x for x in sprint_ids if x])
-        return super(EventLogViewSet, self).apply_filter(qs, raw_filter_args)
+    def _get_filter(self, params):
+        filter_serializer = EventLogFilterSerializer(data=params['additional_params']['filter'])
+        filter_serializer.is_valid(raise_exception=True)
+        return filter_serializer.data
