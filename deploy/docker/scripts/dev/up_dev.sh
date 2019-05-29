70335839b8135912ea82fbbe3fe09f8c8f46d5af
Keenan Adamson <keenan@impd.co.za>
2019-05-29 14:47:50 +0200

    #2066 (sprint open source - sprint1) copy local_settings for dev

--- deploy/docker/scripts/dev/up_dev.sh
+++ deploy/docker/scripts/dev/up_dev.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+API_SETTINGS_SRC="../../config/sample/external_config/api_local_settings.py"
+API_SETTINGS_DST="../../src/implicitdesign/external_config/api_local_settings.py"
+API_SETTINGS_DST_DIR="../../src/implicitdesign/external_config/"
+
+UI_SETTINGS_SRC="../../config/sample/external_config/react_local_settings.js"
+UI_SETTINGS_DST="../../../reimp/public/local_settings/index.html"
+UI_SETTINGS_DST_DIR="../../../reimp/public/local_settings/"
+
 echo "This script only exists for testing. The database is transient and everything will be lost"
 
 cd "`dirname \"$0\"`/"
@@ -20,6 +28,28 @@ fi
 echo "Starting docker compose for transient database"
 cd ${COMPOSE_FOLDER}
 docker-compose -f docker-compose.yml -f docker-compose-dev.transient-db.yml -f docker-compose-dev.yml up -d
+
+if [ ! -e ${API_SETTINGS_DST} ]; then
+    echo "API settings not found"
+    if [! -d ${API_SETTINGS_DST_DIR}]; then
+        mkdir -p ${API_SETTINGS_DST_DIR}
+        touch ${API_SETTINGS_DST_DIR}/__init__.py
+    fi
+    cp ${API_SETTINGS_SRC} ${API_SETTINGS_DST}
+else
+    echo "API settings found"
+fi
+
+if [ ! -e ${UI_SETTINGS_DST} ]; then
+    echo "UI settings not found"
+    if [ ! -d ${UI_SETTINGS_DST_DIR} ]; then
+        mkdir -p ${UI_SETTINGS_DST_DIR}
+    fi
+    cp ${UI_SETTINGS_SRC} ${UI_SETTINGS_DST}
+else
+    echo "UI settings found"
+fi
+
 
 cat << EOF
 
