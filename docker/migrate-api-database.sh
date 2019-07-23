8565acaab3d29d6f17759559e1d07210cd5bba37
Gareth Priede <gtp@impd.co.za>
2019-07-23 09:56:34 +0200

    restored correct migration script

--- docker/migrate-api-database.sh
+++ docker/migrate-api-database.sh
@@ -5,7 +5,7 @@ python manage.py wait_for_db
 
 >&2 echo "migrating api database..."
 
-#yes "yes" | python manage.py migrate
+yes "yes" | python manage.py migrate
 
 >&2 echo "completed migrating api database."
 
