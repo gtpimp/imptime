6a6845ecde976b8295959e2f506ee2473ffbf640
Gareth Priede <gtp@impd.co.za>
2019-07-23 07:44:05 +0200

    whoops on migrate

--- docker/migrate-api-database.sh
+++ docker/migrate-api-database.sh
@@ -5,10 +5,10 @@ python manage.py wait_for_db
 
 >&2 echo "migrating api database..."
 
-yes "yes" | python manage.py migrate
+#yes "yes" | python manage.py migrate
 
 >&2 echo "completed migrating api database."
 
-#python manage.py set_flag db_migrate_complete
+python manage.py set_flag db_migrate_complete
 
 
