b5f08fb3f630cfe1e07e45306864114ee0d0088c
Gareth Priede <gtp@impd.co.za>
2019-10-29 12:16:43 +0200

    fix for downloading files

--- src/lib/file_helper.py
+++ src/lib/file_helper.py
@@ -12,13 +12,14 @@ def download_media(request, url, content_type, filename=None, as_attachment=Fals
         res = requests.get(storage_url)
         response = HttpResponse(res.content)
         response['content-type'] = res.headers['content-type']
-        if as_attachment and filename:
-            response['Content-Disposition'] = 'attachment; filename="%s"' % filename
-        return response
     else:
         with open(os.path.join(settings.MEDIA_ROOT, url)) as f:
             response = HttpResponse(f.read())
-            return response
+        response['content-type'] = content_type
+            
+    if as_attachment and filename:
+        response['Content-Disposition'] = 'attachment; filename="%s"' % filename
+    return response
 
 def prepare_csv(request, filename_prefix):
     response = HttpResponse(content_type='text/csv')
