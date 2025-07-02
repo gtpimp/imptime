464227db8e98b3af4b1828e2820e801c8b2440b5
rory <roryhurt@gmail.com>
2025-07-02 09:51:37 +0200

    fixed old error catch format

--- src/timepiece/caldav_helper.py
+++ src/timepiece/caldav_helper.py
@@ -85,7 +85,8 @@ class CalDavHelper(object):
         uid = self._uid(imptime_event)
         try:
             caldav_event = calendar.event_by_uid(uid)
-        except error.NotFoundError, ex:
+        except error.NotFoundError as ex:
+            logger.exception(ex)
             return None
         except Exception as ex:
             logger.exception(ex)
